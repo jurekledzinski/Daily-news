87ae1577fa1bf0ffc69bcb0113aacaeacfdb78af
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-10 17:20:54 +0200

    frontend: Create routes and assign pages to them

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,7 +1,28 @@
+import CategoriesArticles from '../pages/categories-articles';
+import Dashboard from '../pages/dashboard';
+import DetailsArticle from '../pages/details-article';
+import Home from '../pages/home';
 import { createBrowserRouter } from 'react-router-dom';
 
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: '/',
+    element: <Home />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: 'categories/:category/articles',
+        element: <CategoriesArticles />,
+      },
+      {
+        path: 'categories/:category/articles/article/:id',
+        element: <DetailsArticle />,
+      },
+    ],
   },
 ]);
+
+export default router;
