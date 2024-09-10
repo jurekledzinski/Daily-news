b75dbee18466ada90212003fed5d3c922a0daa17
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-10 17:24:26 +0200

    frontend: Add router provider and routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/AppRouter';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
