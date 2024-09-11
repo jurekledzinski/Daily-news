38d92e5b6116694e21978fda73cc74e6978524c3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-11 18:33:02 +0200

    frontend: Create error message component

---
+++ frontend/src/components/Messages/ErrorMessage.tsx
@@ -0,0 +1,6 @@
+import { ErrorMessageProps } from '../../types';
+import './Messages.css';
+
+export const ErrorMessage = ({ text }: ErrorMessageProps) => {
+  return <p className="error">{text}</p>;
+};
