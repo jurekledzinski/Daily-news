bd55c85de1f4d043dd0a6905a3d72d46a3cd1c26
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-13 18:10:58 +0200

    Frontend: Add grid card

--- frontend/src/components/grid-layout/GridCard.tsx
+++ frontend/src/components/grid-layout/GridCard.tsx
@@ -1,5 +1,45 @@
+import { forwardRef, Ref } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGripVertical } from '@fortawesome/free-solid-svg-icons';
+import { LayoutItem } from '../../types';
 import './GridCard.css';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
 
-export const GridCard = () => {
-  return <div></div>;
+type GridCardProps = {
+  children?: React.ReactNode;
+  className: string;
+  index: number;
+  gridItem: LayoutItem;
+  onClick: (value: string) => void;
 };
+
+export const GridCard = forwardRef<HTMLDivElement, GridCardProps>(
+  (
+    { index, gridItem, onClick, children, ...props },
+    ref: Ref<HTMLDivElement>
+  ) => {
+    const { ui } = gridItem;
+
+    return (
+      <div
+        ref={ref}
+        data-grid={ui}
+        onClick={() => onClick('science')}
+        {...props}
+      >
+        <span className={'handle'} onClick={(e) => e.stopPropagation()}>
+          <FontAwesomeIcon icon={faGripVertical} />
+        </span>
+        <p>
+          {index} -- {ui.i.slice(0, 3)}
+        </p>
+        {children}
+      </div>
+    );
+  }
+);
+
+// onClick={() => {
+//     navigate({ pathname: `categories/science/articles` });
+//   }}
