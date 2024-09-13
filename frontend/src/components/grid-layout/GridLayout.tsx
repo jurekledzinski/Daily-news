a497b64f325618b32938ce33280d6c0d8cef67ef
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-13 16:49:01 +0200

    Frontend:  Refactor grid layout component

--- frontend/src/components/grid-layout/GridLayout.tsx
+++ frontend/src/components/grid-layout/GridLayout.tsx
@@ -4,8 +4,7 @@ import type ReactGridLayout from 'react-grid-layout';
 
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import { TbGridDots } from 'react-icons/tb';
-import { v4 as uuidv4 } from 'uuid';
-import { cloneDeep } from 'lodash';
+import { useControlDashboard } from '../../hooks';
 
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
@@ -14,113 +13,54 @@ import './GridLayout.css';
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 import { LayoutData } from '../../types';
-import { findPosition as findFirstAvailablePosition } from '../../helpers';
-
-const colsLayouts = {
-  lg: 4,
-  md: 3,
-  sm: 2,
-  xs: 1,
-};
 
 export const GridLayout = () => {
   const navigate = useNavigate();
+
   const [data, setData] = useState<LayoutData>({
     lg: [],
     md: [],
     sm: [],
     xs: [],
   });
+
   const [currentBreakPoint, setCurrentBreakPoint] = useState('lg');
 
-  const handleBreakPointChange = (newBreakpoint: string) => {
-    setCurrentBreakPoint(newBreakpoint);
-  };
-
-  const onDrop = (
-    layout: ReactGridLayout.Layout[],
-    newItem: ReactGridLayout.Layout
-  ) => {
-    const tempId = '__dropping-elem__';
-    const newId = uuidv4();
-
-    const item = {
-      ...cloneDeep(newItem),
-      i: newId,
-    };
-
-    const newLayouts = cloneDeep(data);
-
-    const startY = item.y;
-
-    const copy = cloneDeep(layout).map((item) => {
-      if (item.i === tempId) {
-        return { ui: { ...item, i: newId } };
-      }
-      return { ui: { ...item } };
-    });
-
-    for (const breakpoint in data) {
-      if (breakpoint === currentBreakPoint) {
-        newLayouts[breakpoint] = copy;
-      } else {
-        const position = findFirstAvailablePosition({
-          data: newLayouts[breakpoint],
-          cols: colsLayouts[breakpoint as keyof typeof colsLayouts],
-          item,
-          startY,
-        });
-
-        if (position) {
-          item.x = position.x;
-          item.y = position.y;
-          newLayouts[breakpoint] = [
-            ...newLayouts[breakpoint],
-            { ui: { ...item } },
-          ];
-        }
-      }
-    }
-
-    setData(newLayouts);
-  };
-
-  const handleDropStop = (layout: ReactGridLayout.Layout[]) => {
-    setData((prev) => ({
-      ...prev,
-      [currentBreakPoint]: prev[currentBreakPoint].map((item) => {
-        const sameItem = layout.find((el) => el.i === item.ui.i);
-
-        if (sameItem) {
-          return { ui: sameItem, id: sameItem.i };
-        }
-
-        return item;
-      }),
-    }));
-  };
-
-  const handleResizeStop = (layout: ReactGridLayout.Layout[]) => {
-    setData((prev) => ({
-      ...prev,
-      [currentBreakPoint]: prev[currentBreakPoint].map((item) => {
-        const sameItem = layout.find((el) => el.i === item.ui.i);
-
-        if (sameItem) {
-          return { ui: sameItem, id: sameItem.i };
-        }
-
-        return item;
-      }),
-    }));
-  };
+  const {
+    handleBreakPointChange,
+    handleDrop,
+    handleDropStop,
+    handleResizeStop,
+  } = useControlDashboard({
+    currentBreakPoint,
+    data,
+    onChangeBreakpoint: (breakpoint) => {
+      setCurrentBreakPoint(breakpoint);
+    },
+    onDrop: (newLayouts) => {
+      setData(newLayouts);
+    },
+    onDropStop: (newLayouts) => {
+      setData(newLayouts);
+    },
+    onResizeStop: (newLayouts) => {
+      setData(newLayouts);
+    },
+  });
 
   return (
     <div className="wrapper">
       <ResponsiveGridLayout
-        onDragStop={handleDropStop}
-        onResizeStop={handleResizeStop}
+        breakpoints={{
+          lg: 1200 - 266,
+          md: 996 - 266,
+          sm: 768 - 266,
+          xs: 500 - 266,
+        }}
         className="layout"
+        cols={{ lg: 4, md: 3, sm: 2, xs: 1 }}
+        draggableHandle=".handle"
+        isDroppable={true}
         layouts={
           Object.fromEntries(
             Object.entries(data).map((layout) => [
@@ -129,19 +69,12 @@ export const GridLayout = () => {
             ])
           ) as ReactGridLayout.Layouts
         }
-        breakpoints={{
-          lg: 1200 - 266,
-          md: 996 - 266,
-          sm: 768 - 266,
-          xs: 500 - 266,
-        }}
-        cols={{ lg: 4, md: 3, sm: 2, xs: 1 }}
-        rowHeight={30}
-        onDrop={onDrop}
+        onDragStop={handleDropStop}
+        onResizeStop={handleResizeStop}
+        onDrop={handleDrop}
         onBreakpointChange={handleBreakPointChange}
-        draggableHandle=".handle"
-        isDroppable={true}
         onDropDragOver={() => ({ w: 1, h: 4 })}
+        rowHeight={30}
       >
         {data[currentBreakPoint as keyof typeof data].map((item, index) => {
           return (
