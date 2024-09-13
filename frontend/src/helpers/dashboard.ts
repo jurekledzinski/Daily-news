e2d06ec6c11b7b5e105e3eb5057df0f230f5d703
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-13 12:16:18 +0200

    Frontend: Create folder for helpers

---
+++ frontend/src/helpers/dashboard.ts
@@ -0,0 +1,71 @@
+import { LayoutData, LayoutItem } from '../types';
+
+type fnFindPositionParams = {
+  data: LayoutData[0];
+  cols: number;
+  item: LayoutItem['ui'];
+  startY: number;
+};
+
+type fnCanPlaceItemParams = {
+  layout: LayoutData[0];
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+};
+
+//     for (let y = startY; y < 100; y++) {
+
+export const canPlaceItem = (params: fnCanPlaceItemParams) => {
+  const { layout, h, w, x, y } = params;
+
+  for (let i = 0; i < layout.length; i++) {
+    const l = layout[i];
+    if (
+      x < l.ui.x + l.ui.w &&
+      x + w > l.ui.x &&
+      y < l.ui.y + l.ui.h &&
+      y + h > l.ui.y
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const findPosition = (params: fnFindPositionParams) => {
+  const { cols, data, item } = params;
+
+  for (let y = 0; y < 100; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (canPlaceItem({ layout: data, x, y, w: item.w, h: item.h })) {
+        return { x, y };
+      }
+    }
+  }
+  return null;
+};
+
+// const findFirstAvailablePosition = (
+//   data: LayoutData[0],
+//   cols: number,
+//   item: LayoutItem['ui'],
+//   startY: number
+// ) => {
+//   console.log('find data', data);
+//   console.log('find cols', cols);
+//   console.log('find item', item);
+//   console.log('find startY', startY);
+
+//   //     for (let y = startY; y < 100; y++) {
+
+//   for (let y = 0; y < 100; y++) {
+//     for (let x = 0; x < cols; x++) {
+//       if (canPlaceItem(data, x, y, item.w, item.h)) {
+//         return { x, y };
+//       }
+//     }
+//   }
+//   return null;
+// };
