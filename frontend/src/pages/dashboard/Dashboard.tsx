b97a82261bf0ca02fafbfb29eb18da53e5440d1c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-13 12:17:32 +0200

    Frontend: Set height in dashboard section page

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,12 @@
+// import { useState } from 'react';
+import GridLayout from '../../components/grid-layout';
+import './Dashboard.css';
+// import { LayoutData } from '../../types';
+
 export const Dashboard = () => {
-  return <section className="section">Dashboard</section>;
+  return (
+    <section className="section">
+      <GridLayout />
+    </section>
+  );
 };
--- frontend/src/pages/dashboard/index.ts
+++ frontend/src/pages/dashboard/index.ts
@@ -1,2 +1,3 @@
 export * from './Dashboard';
+export * from './Dashboard.css';
 export { Dashboard as default } from './Dashboard';
