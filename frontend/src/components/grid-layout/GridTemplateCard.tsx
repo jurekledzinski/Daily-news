d94f032f982b17710af60ac862b5f8ea7df52707
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-16 14:00:49 +0200

    frontend: update GridTemplateCard

--- frontend/src/components/grid-layout/GridTemplateCard.tsx
+++ frontend/src/components/grid-layout/GridTemplateCard.tsx
@@ -1,5 +1,39 @@
 import './GridTemplateCard.css';
+import { categories } from '../../dummy-api';
 
-export const GridTemplateCard = () => {
-  return <div></div>;
+export type GridTemplateCardProps = {
+  data: (typeof categories)[0];
+  isDisabled: boolean;
+};
+
+export const GridTemplateCard = ({
+  data,
+  isDisabled,
+}: GridTemplateCardProps) => {
+  return (
+    <div
+      className={
+        isDisabled
+          ? `grid-template-card grid-template-card--disabled`
+          : 'grid-template-card'
+      }
+      draggable={true}
+      onDragStart={(e) => {
+        if (isDisabled) e.preventDefault();
+
+        const transferData = {
+          id: data.id,
+          title: data.webTitle,
+        };
+
+        e.dataTransfer.setData('text/plain', `${JSON.stringify(transferData)}`);
+      }}
+      onTouchStart={(e: React.TouchEvent) => {
+        console.log('touch start', e);
+      }}
+      unselectable="on"
+    >
+      <h6>{data.webTitle}</h6>
+    </div>
+  );
 };
