62be157cc28a8d7c4cb09f54dd66beb74e1ac362
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-16 14:01:39 +0200

    frontend: update Dashboard

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,33 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import GridLayout from '../../components/grid-layout';
+import { GridTemplateCard } from '../../components/grid-layout';
+import { categories } from '../../dummy-api';
 import './Dashboard.css';
-// import { LayoutData } from '../../types';
+import { LayoutData } from '../../types';
 
 export const Dashboard = () => {
+  const [data, setData] = useState<LayoutData>({
+    lg: [],
+    md: [],
+    sm: [],
+    xs: [],
+  });
+
   return (
-    <section className="section">
-      <GridLayout />
+    <section className="section section--dashboard">
+      <GridLayout data={data} setData={setData} />
+      <div className="aside">
+        {categories.map((section) => {
+          const card = data.lg.find((cardItem) => cardItem.id === section.id);
+          return (
+            <GridTemplateCard
+              data={section}
+              key={section.id}
+              isDisabled={card ? true : false}
+            />
+          );
+        })}
+      </div>
     </section>
   );
 };
