88f7042b05065fac56dac8b14eb0982c8b14dd69
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-18 17:18:06 +0200

    frontend: Create grid articles component

---
+++ frontend/src/components/grid-articles/GridArticles.tsx
@@ -0,0 +1,18 @@
+import './GridArticles.css';
+
+export const GridArticles = () => {
+  return (
+    <div className="grid-articles">
+      <div className="grid-articles__card">1</div>
+      <div className="grid-articles__card">2</div>
+      <div className="grid-articles__card">3</div>
+      <div className="grid-articles__card">4</div>
+      <div className="grid-articles__card">5</div>
+      <div className="grid-articles__card">6</div>
+      <div className="grid-articles__card">7</div>
+      <div className="grid-articles__card">8</div>
+      <div className="grid-articles__card">9</div>
+      <div className="grid-articles__card">10</div>
+    </div>
+  );
+};
