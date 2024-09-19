18a3c2aafd2c6fc9a64866fc8da112accdcb0ddf
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-19 18:07:26 +0200

    frontend: set data grid dashboard to local storage and load data on refresh

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
-import { GridLayout } from '../../components/pages';
+import './Dashboard.css';
+import { useEffect, useState } from 'react';
+import { GridLayout, LayoutItem } from '../../components/pages';
 import { GridTemplateCard } from '../../components/pages';
 import { categories } from '../../dummy-api';
-import './Dashboard.css';
-import { LayoutData } from '../../types';
+import { LayoutData } from '../../components/pages';
+
+export type LocalData = LayoutData[0][0] & { articles: string[] };
+
+type Arr = {
+  id: string;
+  title: string;
+  ui: {
+    [P: string]: LayoutItem['ui'];
+  };
+  articles: string[] | [];
+};
 
 export const Dashboard = () => {
   const [data, setData] = useState<LayoutData>({
@@ -13,9 +24,79 @@ export const Dashboard = () => {
     xs: [],
   });
 
+  const handleSetData = (dataLayout: LayoutData) => {
+    const localData: Arr[] =
+      JSON.parse(localStorage.getItem('categories') ?? 'null') || [];
+
+    const transformData = Object.entries(dataLayout).reduce<Arr[]>(
+      (acc, curr) => {
+        curr[1].forEach((category) => {
+          const isExist = acc.find((item) => item.id === category.id);
+          const inLocal = localData.find((i) => i.id === category.id);
+
+          if (isExist) {
+            const accData = acc.map((i) => {
+              if (i.id === category.id) {
+                return { ...i, ui: { ...i.ui, [curr[0]]: category.ui } };
+              }
+              return i;
+            });
+
+            return (acc = accData);
+          }
+
+          const singleCategory = {
+            id: category.id ?? '',
+            title: category.title,
+            ui: { [curr[0]]: category.ui },
+            articles: localData.length ? inLocal?.articles ?? [] : [],
+          };
+
+          const accData = [...acc, singleCategory];
+          return (acc = accData);
+        });
+
+        return acc;
+      },
+      []
+    );
+
+    localStorage.setItem('categories', JSON.stringify(transformData));
+    setData(dataLayout);
+  };
+
+  useEffect(() => {
+    const localData: Arr[] =
+      JSON.parse(localStorage.getItem('categories') ?? 'null') || [];
+    if (!localData.length) return;
+
+    const transformedData = localData.reduce<LayoutData>((acc, curr) => {
+      let tempAcc: LayoutData = {};
+
+      Object.entries(curr.ui).forEach((item) => {
+        tempAcc = {
+          ...tempAcc,
+          [item[0]]: [{ id: curr.id ?? '', title: curr.title, ui: item[1] }],
+        };
+      });
+
+      if (Object.keys(acc).length) {
+        const accData = Object.entries(acc).map((i) => [
+          i[0],
+          [...i[1], tempAcc[i[0]][0]],
+        ]);
+        return (acc = Object.fromEntries(accData));
+      } else {
+        return (acc = tempAcc);
+      }
+    }, {});
+
+    setData(transformedData);
+  }, []);
+
   return (
     <section className="section section--dashboard">
-      <GridLayout data={data} setData={setData} />
+      <GridLayout data={data} setData={handleSetData} />
       <div className="aside">
         {categories.map((section) => {
           const card = data.lg.find((cardItem) => cardItem.id === section.id);
