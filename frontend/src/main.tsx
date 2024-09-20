82ab6a55c12e1920352ffe95fc38309819d2ddfb
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-20 14:25:19 +0200

    frontend: sort imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,9 @@
-import './index.css';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import router from './routes/AppRouter';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
 
 const queryClient = new QueryClient();
 
