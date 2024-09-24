4c0045becd20c14f730ff594a84fb746987ecbf0
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-24 19:50:13 +0200

    frontend: Create details article component

--- frontend/src/pages/details-article/DetailsArticle.tsx
+++ frontend/src/pages/details-article/DetailsArticle.tsx
@@ -1,10 +1,35 @@
-import { useParams } from 'react-router-dom';
+import { ArticleDetails } from '../../components/pages';
+import { getDetailsArticleImageData } from '../../helpers';
+import { loaderDetailsArticle } from '../../api';
+import { useLoaderData } from 'react-router-dom';
 
 export const DetailsArticle = () => {
-  const { category, id } = useParams();
+  const data = useLoaderData() as Awaited<
+    ReturnType<ReturnType<typeof loaderDetailsArticle>>
+  >;
+
+  console.log('data', data);
+
   return (
-    <section className="section">
-      Details article {category} {id}
+    <section className="section section--details-article">
+      <ArticleDetails
+        data={{
+          id: data.response.content.id,
+          sectionId: data.response.content.sectionId,
+          title: data.response.content.webTitle,
+          content: data.response.content.fields.body,
+          trailText: data.response.content.fields.trailText,
+          webPublicationDate: data.response.content.webPublicationDate,
+          image: getDetailsArticleImageData(data.response.content.elements)
+            .image,
+          altText: getDetailsArticleImageData(data.response.content.elements)
+            .altText,
+          caption: getDetailsArticleImageData(data.response.content.elements)
+            .caption,
+          credit: getDetailsArticleImageData(data.response.content.elements)
+            .credit,
+        }}
+      />
     </section>
   );
 };
