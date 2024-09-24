fe2a79cb5959c5ebc0c92c633865aa15e54d465d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-24 19:55:01 +0200

    frontend: remove main element to set correctly heights

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,14 +1,24 @@
 import { Header } from '../../components/pages';
-import { Outlet } from 'react-router-dom';
+import { loaderCategories } from '../../api';
+import { Outlet, useLoaderData } from 'react-router-dom';
 import './Home.css';
 
 export const Home = () => {
+  const data = useLoaderData() as Awaited<
+    ReturnType<ReturnType<typeof loaderCategories>>
+  >;
+
   return (
     <div className="container">
       <Header />
-      <main className="main">
-        <Outlet />
-      </main>
+      <Outlet
+        context={{
+          categories: (data.response.results ?? []).map((c) => ({
+            id: c.id,
+            title: c.webTitle,
+          })),
+        }}
+      />
       <footer className="footer">
         All rights reserved © {new Date().getFullYear()}
       </footer>
