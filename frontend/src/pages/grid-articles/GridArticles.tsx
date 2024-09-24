c6c886adda064779edc778f59413441cdae2101a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-24 19:51:51 +0200

    frontend: transform data api for grid articles

--- frontend/src/pages/grid-articles/GridArticles.tsx
+++ frontend/src/pages/grid-articles/GridArticles.tsx
@@ -1,33 +1,71 @@
 import { Card } from '../../components/pages';
-import { useOutletContext, useParams } from 'react-router-dom';
+import { useLoaderData, useOutletContext } from 'react-router-dom';
 import './GridArticles.css';
+
+import { loaderArticles } from '../../api';
 
 type GridArticlesContext = {
   handleAddSubArticle: (value: { id: string; title: string }) => void;
 };
 
-const article = {
-  id: 'id',
-  title: 'title',
-  content: 'content',
-  image:
-    'https://cdn.pixabay.com/photo/2018/06/12/19/59/football-3471371_1280.jpg',
-};
-
 export const GridArticles = () => {
-  const { category } = useParams();
+  //   const { category } = useParams();
+
   const { handleAddSubArticle } = useOutletContext<GridArticlesContext>();
-  console.log('GRID', category);
+
+  const data = useLoaderData() as Awaited<
+    ReturnType<ReturnType<typeof loaderArticles>>
+  >;
 
   return (
     <div className="grid-articles">
-      {Array.from({ length: 10 }).map((_, index) => (
-        <Card
-          key={index}
-          handleAddSubArticle={handleAddSubArticle}
-          article={article}
-        />
-      ))}
+      {data.response.results
+        .map((article) => {
+          const transformedData = {
+            content: article.fields.trailText,
+            id: article.id,
+            title: article.webTitle,
+            sectionId: article.sectionId,
+          };
+
+          if (article.elements && article.elements.length) {
+            const main = article.elements.sort((a, b) =>
+              a.relation.localeCompare(b.relation)
+            );
+
+            const mainImage =
+              main[0].assets && main[0].assets.length
+                ? main[0].assets.sort(
+                    (a, b) =>
+                      parseFloat(b.typeData.width) -
+                      parseFloat(a.typeData.width)
+                  )[main[0].assets.length - 2]
+                : '';
+
+            return {
+              ...transformedData,
+              image: mainImage ? mainImage.file : '',
+              altText: mainImage ? mainImage.typeData.altText : '',
+              credit: mainImage ? mainImage.typeData.credit : '',
+              caption: mainImage ? mainImage.typeData.caption : '',
+            };
+          }
+
+          return {
+            ...transformedData,
+            image: '',
+            altText: '',
+            credit: '',
+            caption: '',
+          };
+        })
+        .map((article) => (
+          <Card
+            key={article.id}
+            handleAddSubArticle={handleAddSubArticle}
+            article={article}
+          />
+        ))}
     </div>
   );
 };
