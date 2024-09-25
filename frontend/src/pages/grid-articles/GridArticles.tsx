387e770b8427ad48e9ca792ad41b1ba203ddc7da
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-25 19:33:21 +0200

    frontend: Add fetch on scroll articles

--- frontend/src/pages/grid-articles/GridArticles.tsx
+++ frontend/src/pages/grid-articles/GridArticles.tsx
@@ -1,25 +1,49 @@
 import { Card } from '../../components/pages';
-import { useLoaderData, useOutletContext } from 'react-router-dom';
+import { IArticles, loaderArticles } from '../../api';
+import { uniqBy } from 'lodash';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useFetchOnScroll } from '../../hooks';
+import { UseOutletContext } from '../../types/global';
 import './GridArticles.css';
-
-import { loaderArticles } from '../../api';
-
-type GridArticlesContext = {
-  handleAddSubArticle: (value: { id: string; title: string }) => void;
-};
+import {
+  useLoaderData,
+  useOutletContext,
+  useSearchParams,
+} from 'react-router-dom';
 
 export const GridArticles = () => {
-  //   const { category } = useParams();
-
-  const { handleAddSubArticle } = useOutletContext<GridArticlesContext>();
+  const context = useOutletContext<UseOutletContext>();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [state, setState] = useState<IArticles[]>([]);
+  const page = useRef<number>(1);
 
   const data = useLoaderData() as Awaited<
     ReturnType<ReturnType<typeof loaderArticles>>
   >;
 
+  useFetchOnScroll({
+    onChangeVisible: useCallback(
+      (value) => {
+        const currentPage = Number(searchParams.get('page'));
+        if (data.response.pages === currentPage) return;
+
+        page.current += 1;
+
+        if (value) setSearchParams({ page: `${page.current}` });
+      },
+      [data.response.pages, searchParams, setSearchParams]
+    ),
+    target: context.footerRef,
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    setState((prev) => uniqBy([...prev, ...data.response.results], 'id'));
+  }, [data]);
+
   return (
     <div className="grid-articles">
-      {data.response.results
+      {state
         .map((article) => {
           const transformedData = {
             content: article.fields.trailText,
@@ -62,10 +86,51 @@ export const GridArticles = () => {
         .map((article) => (
           <Card
             key={article.id}
-            handleAddSubArticle={handleAddSubArticle}
+            handleAddSubArticle={context.handleAddSubArticle}
             article={article}
           />
         ))}
     </div>
   );
 };
+
+// TODO: create function to return transformed data
+
+function transformData(article: IArticles) {
+  const transformedData = {
+    content: article.fields.trailText,
+    id: article.id,
+    title: article.webTitle,
+    sectionId: article.sectionId,
+  };
+
+  if (article.elements && article.elements.length) {
+    const main = article.elements.sort((a, b) =>
+      a.relation.localeCompare(b.relation)
+    );
+
+    const mainImage =
+      main[0].assets && main[0].assets.length
+        ? main[0].assets.sort(
+            (a, b) =>
+              parseFloat(b.typeData.width) - parseFloat(a.typeData.width)
+          )[main[0].assets.length - 2]
+        : '';
+
+    return {
+      ...transformedData,
+      image: mainImage ? mainImage.file : '',
+      altText: mainImage ? mainImage.typeData.altText : '',
+      credit: mainImage ? mainImage.typeData.credit : '',
+      caption: mainImage ? mainImage.typeData.caption : '',
+    };
+  }
+
+  return {
+    ...transformedData,
+    image: '',
+    altText: '',
+    credit: '',
+    caption: '',
+  };
+}
