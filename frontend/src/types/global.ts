b9820fb483ec07ed255876ed370468640b2b111e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-27 14:15:22 +0200

    frontend: move types for useScrollToggle to global types

--- frontend/src/types/global.ts
+++ frontend/src/types/global.ts
@@ -2,3 +2,11 @@ export type UseOutletContext = {
   footerRef: React.MutableRefObject<HTMLDivElement | null>;
   handleAddSubArticle: (value: { id: string; title: string }) => void;
 };
+
+export type UseScrollToggleProps = {
+  target: React.MutableRefObject<HTMLDivElement | null>;
+  onChangeVisible: (value: boolean) => void;
+  root?: IntersectionObserverInit['root'];
+  rootMargin?: IntersectionObserverInit['rootMargin'];
+  threshold?: IntersectionObserverInit['threshold'];
+};
