cb8df8e91a8eda8cc242da86aeb4c16fb5043da5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-29 12:37:43 +0200

    frontend: get ref from tabs container component

--- frontend/src/components/shared/tabs/TabsListContainer.tsx
+++ frontend/src/components/shared/tabs/TabsListContainer.tsx
@@ -1,12 +1,13 @@
+import { forwardRef, Ref } from 'react';
 import { TabsListContainerProps } from './types';
 
-export const TabsListConainer = ({
-  children,
-  ...props
-}: TabsListContainerProps) => {
+export const TabsListConainer = forwardRef<
+  HTMLDivElement,
+  TabsListContainerProps
+>(({ children, ...props }, ref: Ref<HTMLDivElement>) => {
   return (
-    <div className="tabs__list-container" {...props}>
+    <div className="tabs__list-container" {...props} ref={ref}>
       {children}
     </div>
   );
-};
+});
