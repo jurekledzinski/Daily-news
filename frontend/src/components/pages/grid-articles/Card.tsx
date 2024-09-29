20a8909ad63f084456a263a321fa95d2dcf8a810
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-09-29 12:36:35 +0200

    frontend: get ref from card component

--- frontend/src/components/pages/grid-articles/Card.tsx
+++ frontend/src/components/pages/grid-articles/Card.tsx
@@ -5,32 +5,37 @@ import { Image } from '../../shared';
 import { Link } from 'react-router-dom';
 import { sanitizeContent } from '../../../helpers';
 import './Card.css';
+import { forwardRef, Ref } from 'react';
 
-export const Card = ({ article, handleAddSubArticle }: CardProps) => {
-  const { content, id, image, title } = article;
-  const cleanContent = sanitizeContent(content);
+export const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({ article, handleAddSubArticle }, ref: Ref<HTMLDivElement>) => {
+    const { content, id, image, title } = article;
+    const cleanContent = sanitizeContent(content);
 
-  return (
-    <div className="card">
-      <div className="card__header">
-        <Link
-          to={`article/${encodeURIComponent(id)}`}
-          onClick={() => handleAddSubArticle({ id, title })}
-        >
-          {image ? (
-            <Image className="image" altText={title} src={image} />
-          ) : (
-            <FontAwesomeIcon icon={faImage} />
-          )}
-        </Link>
+    return (
+      <div className="card" ref={ref}>
+        <div className="card__header">
+          <Link
+            to={`article/${encodeURIComponent(id)}`}
+            onClick={(e) => {
+              handleAddSubArticle({ id, title, scroll: e.pageY });
+            }}
+          >
+            {image ? (
+              <Image className="image" altText={title} src={image} />
+            ) : (
+              <FontAwesomeIcon icon={faImage} />
+            )}
+          </Link>
+        </div>
+        <div className="card__body">
+          <h6 className="card__title">{title}</h6>
+          <p
+            className="card__content"
+            dangerouslySetInnerHTML={{ __html: cleanContent }}
+          ></p>
+        </div>
       </div>
-      <div className="card__body">
-        <h6 className="card__title">{title}</h6>
-        <p
-          className="card__content"
-          dangerouslySetInnerHTML={{ __html: cleanContent }}
-        ></p>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+);
