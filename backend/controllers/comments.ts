627ce99018bf9d85d94be266cdc31d0718e41e7c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-11 18:25:38 +0200

    backend: Work comments controllers

--- backend/controllers/comments.ts
+++ backend/controllers/comments.ts
@@ -1,10 +1,10 @@
 import CustomError from '../error/error';
 import { CommentSchema, IComment } from '../models/comments';
 import { getCollectionDb } from '../config/db';
-import { WithId } from 'mongodb';
 import { Request, Response } from 'express';
 import { transformDocument } from '../helpers/transformData';
 import { tryCatch } from '../helpers/tryCatch';
+import { ObjectId, WithId } from 'mongodb';
 
 const collection = getCollectionDb<IComment>('comments');
 
@@ -25,10 +25,32 @@ export const getComments = tryCatch<IComment[]>(
     });
 
     const result = await collection
-      .find({ idArticle: article_id, parentCommentId: null })
-      .sort({ createdAt: 1 })
-      .skip(skipCount)
-      .limit(pageSize)
+      .aggregate<IComment>([
+        { $match: { idArticle: article_id, parentCommentId: null } },
+        {
+          $lookup: {
+            from: 'comments',
+            let: { topCommentId: { $toString: '$_id' } },
+            pipeline: [
+              {
+                $match: {
+                  $expr: { $eq: ['$parentCommentId', '$$topCommentId'] },
+                },
+              },
+            ],
+            as: 'replies',
+          },
+        },
+        {
+          $addFields: {
+            replyCount: { $size: '$replies' },
+          },
+        },
+        { $project: { replies: 0 } },
+        { $sort: { createdAt: -1 } },
+        { $skip: skipCount },
+        { $limit: pageSize },
+      ])
       .toArray();
 
     const formatResults = transformDocument<WithId<IComment>>(result);
@@ -48,20 +70,13 @@ export const getCommentReplies = tryCatch<IComment[]>(
       throw new CustomError('Internal server error', 500);
     }
 
+    console.log('req.query get replies', req.query);
+
     const article_id = decodeURIComponent(req.params.article_id);
     const comment_id = req.params.comment_id;
-    const page = req.query.page ?? '1';
+    const page = req.query.page_reply ?? '1';
     const pageSize = 10;
     const skipCount = (parseInt(page.toString()) - 1) * pageSize;
-
-    if (comment_id === 'initial') {
-      return res.status(200).json({
-        success: true,
-        payload: { results: [] },
-        totalPages: 0,
-        page: parseInt(page.toString()),
-      });
-    }
 
     const total = await collection.countDocuments({
       idArticle: article_id,
@@ -69,21 +84,44 @@ export const getCommentReplies = tryCatch<IComment[]>(
     });
 
     const result = await collection
-      .find({ idArticle: article_id, parentCommentId: comment_id })
-      .sort({ createdAt: 1 })
-      .skip(skipCount)
-      .limit(pageSize)
+      .aggregate<IComment>([
+        { $match: { idArticle: article_id, parentCommentId: comment_id } },
+        {
+          $lookup: {
+            from: 'comments',
+            let: { topCommentId: { $toString: '$_id' } },
+            pipeline: [
+              {
+                $match: {
+                  $expr: { $eq: ['$parentCommentId', '$$topCommentId'] },
+                },
+              },
+            ],
+            as: 'replies',
+          },
+        },
+        {
+          $addFields: {
+            replyCount: { $size: '$replies' },
+          },
+        },
+        { $project: { replies: 0 } },
+        { $sort: { createdAt: -1 } },
+        { $skip: skipCount },
+        { $limit: pageSize },
+      ])
       .toArray();
 
-    const formatResults = result.map((i) => ({ ...i, id: i._id }));
+    const formatResults = transformDocument<WithId<IComment>>(result);
 
-    console.log('formatResults', formatResults);
+    console.log('all replies for parentId', comment_id, total);
 
     return res.status(200).json({
       payload: { result: formatResults },
       success: true,
       totalPages: Math.ceil(total / pageSize),
       page: parseInt(page.toString()),
+      replyCount: total,
     });
   }
 );
@@ -99,9 +137,29 @@ export const createComment = tryCatch(async (req: Request, res: Response) => {
 
   await collection.insertOne({
     ...req.body,
-    likes: parseInt(req.body.likes),
-    parentCommentId: parentId ? null : parentId,
+    parentCommentId: parentId ? null : req.body.parentCommentId,
   });
 
   return res.status(200).json({ success: true });
 });
+
+export const updateCommentLikes = tryCatch(
+  async (req: Request, res: Response) => {
+    if (!collection) {
+      throw new CustomError('Internal server error', 500);
+    }
+
+    const articleId = decodeURIComponent(req.params.article_id);
+    const commentId = req.params.comment_id;
+
+    const a = await collection.updateOne(
+      {
+        _id: new ObjectId(commentId),
+        idArticle: articleId,
+      },
+      { $inc: req.body }
+    );
+
+    return res.status(200).json({ success: true });
+  }
+);
