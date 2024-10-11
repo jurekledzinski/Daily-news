239dc74afbc19a356b87a9bd4d7d378285bcb40b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-11 18:26:02 +0200

    backend: Add update likes route

--- backend/routes/comments.ts
+++ backend/routes/comments.ts
@@ -4,10 +4,12 @@ import {
   getComments,
   getCommentReplies,
   createComment,
+  updateCommentLikes,
 } from '../controllers/comments';
 
 router.route('/:article_id').get(getComments);
 router.route('/:article_id/:comment_id').get(getCommentReplies);
 router.route('/create').post(createComment);
+router.route('/likes/:article_id/:comment_id').patch(updateCommentLikes);
 
 export default router;
