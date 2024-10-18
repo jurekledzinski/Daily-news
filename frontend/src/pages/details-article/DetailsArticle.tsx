e2128203059c36579dfdda662c81203055a4bf93
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-18 13:34:59 +0200

    frontend: refactor code to load comments

--- frontend/src/pages/details-article/DetailsArticle.tsx
+++ frontend/src/pages/details-article/DetailsArticle.tsx
@@ -1,33 +1,41 @@
 import { ArticleDetails } from '../../components/pages';
-import { cloneDeep, uniqBy } from 'lodash';
-import { CommentWithReplies } from '../../components/shared';
-import { getDetailsArticleImageData } from '../../helpers';
+import { cloneDeep } from 'lodash';
+import { CommentsWithReplies } from '../../components/shared';
+import {
+  getDetailsArticleImageData,
+  updateNestedRepliesLikes,
+} from '../../helpers';
 import { loaderDetailsArticle } from '../../api';
-import { useAddComment, useScrollToggle, useUpdateLikes } from '../../hooks';
-import { useEffect, useRef, useState } from 'react';
-import { UseOutletContext } from '../../types/global';
+import {
+  useAddComment,
+  useFetchOnScroll,
+  useLoadComments,
+  useUpdateLikes,
+} from '../../hooks';
+import { useCallback, useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import {
   Params,
   useLoaderData,
-  useOutletContext,
   useParams,
+  useSearchParams,
 } from 'react-router-dom';
 
 type LoaderData = Awaited<ReturnType<ReturnType<typeof loaderDetailsArticle>>>;
 
 export const DetailsArticle = () => {
   const articleHeaderRef = useRef<HTMLDivElement | null>(null);
-  const [state, setState] = useState<CommentWithReplies[]>([]);
+  const [stateComments, setStateComments] = useState<
+    Record<string, CommentsWithReplies[]>
+  >({});
   const { id } = useParams() as Params;
   const articleId = decodeURIComponent(id ?? '');
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const data = useLoaderData() as LoaderData;
   const article = data.detailsArticle.response.content;
 
-  const context = useOutletContext<UseOutletContext>();
-
-  const methodSubmit = useAddComment({
+  const methodSubmitComment = useAddComment({
     artId: articleId,
     user: `user-${uuidv4().slice(0, 6)}`,
     userId: uuidv4(),
@@ -35,9 +43,10 @@ export const DetailsArticle = () => {
 
   const methodSubmitLike = useUpdateLikes({
     onLikes: (data) => {
+      if (!id || !stateComments[id]) return;
       const { commentId, likes } = data;
 
-      const update = cloneDeep(state).map((topComment) => ({
+      const update = cloneDeep(stateComments[id]).map((topComment) => ({
         ...topComment,
         likes:
           commentId === topComment.id
@@ -49,105 +58,36 @@ export const DetailsArticle = () => {
             : topComment.replies,
       }));
 
-      setState(update);
+      setStateComments((prev) => ({ ...prev, [id]: update }));
     },
   });
 
-  useScrollToggle({
-    onChangeVisible: (value) => {
-      if (!context.headerRef.current) return;
-      if (!context.tabsListContainerRef.current) return;
+  useFetchOnScroll({
+    onChangeVisible: useCallback(
+      (value) => {
+        if (!value) return;
 
-      if (value) {
-        context.headerRef.current.classList.remove('slide');
-        context.tabsListContainerRef.current.classList.remove('slide');
-      } else {
-        context.headerRef.current.classList.add('slide');
-        context.tabsListContainerRef.current.classList.add('slide');
-      }
-    },
-    target: articleHeaderRef,
-    threshold: [0.5, 1.0],
+        const currentPage = Number(searchParams.get('page')) || 1;
+        if (data.comments.totalPages === currentPage) return;
+
+        const nextPage = currentPage + 1;
+        setSearchParams({ page: `${nextPage}` }, { replace: true });
+      },
+      [data.comments.totalPages, setSearchParams, searchParams]
+    ),
   });
 
-  console.log('DATA ---', data);
-
-  useEffect(() => {
-    const comments = data.comments.payload.result as CommentWithReplies[];
-    const replies = data.commentReplies.payload.result as CommentWithReplies[];
-    const pageReply = data.commentReplies.page;
-    const totalReplyPages = data.commentReplies.totalPages;
-    const replyCount = data.commentReplies.replyCount ?? 0;
-
-    setState((prev) => {
-      console.log('previos', prev);
-      console.log('comments', comments);
-      console.log('replies', replies);
-      return uniqBy([...prev, ...comments], 'id')
-        .map((topComment) => {
-          const current = comments.find((i) => i.id === topComment.id);
-          const hasReply = replies.some(
-            (reply) => reply.parentCommentId === topComment.id
-          );
-
-          if (hasReply) {
-            return {
-              ...topComment,
-              page: data.comments.page,
-              totalPages: data.comments.totalPages,
-              replyCount: current?.replyCount ?? topComment.replyCount,
-              pageReply: data.commentReplies.page,
-              totalReplyPages: data.commentReplies.totalPages,
-              replies: uniqBy(
-                [...(topComment.replies ?? []), ...replies].sort(
-                  (a, b) =>
-                    new Date(b.createdAt).getTime() -
-                    new Date(a.createdAt).getTime()
-                ),
-                'id'
-              ),
-            };
-          } else {
-            const parentId = replies[0]?.parentCommentId ?? '';
-
-            return {
-              ...topComment,
-              page: data.comments.page,
-              totalPages: data.comments.totalPages,
-              replyCount: current?.replyCount ?? topComment.replyCount,
-              pageReply: data.commentReplies.page,
-              totalReplyPages: data.commentReplies.totalPages,
-              replies: uniqBy(
-                udpatedNestedReplies(
-                  topComment,
-                  parentId,
-                  replies,
-                  pageReply,
-                  totalReplyPages,
-                  replyCount
-                ).sort(
-                  (a, b) =>
-                    new Date(b.createdAt).getTime() -
-                    new Date(a.createdAt).getTime()
-                ),
-                'id'
-              ),
-            };
-          }
-        })
-        .sort(
-          (a, b) =>
-            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-        );
-    });
-  }, [data.comments, data.commentReplies]);
-
-  console.log('state ----', state);
+  useLoadComments({
+    dataComments: data.comments,
+    dataCommentReplies: data.commentReplies,
+    id,
+    setStateComments,
+  });
 
   return (
     <section className="section section--details-article">
       <ArticleDetails
-        comments={state}
+        comments={id && stateComments[id] ? stateComments[id] : []}
         headerRef={articleHeaderRef}
         data={{
           id: article.id,
@@ -161,78 +101,18 @@ export const DetailsArticle = () => {
           caption: getDetailsArticleImageData(article.elements).caption,
           credit: getDetailsArticleImageData(article.elements).credit,
         }}
-        methodSubmit={methodSubmit}
+        methodSubmitComment={methodSubmitComment}
         methodSubmitLike={methodSubmitLike}
+        onShowReplies={async (commentId) => {
+          setSearchParams({ comment_id: commentId, page_reply: '1' });
+        }}
+        onShowMoreReplies={(parentCommentId, page) => {
+          const currentParams = new URLSearchParams(searchParams);
+          currentParams.set('comment_id', parentCommentId);
+          currentParams.set('page_reply', (page + 1).toString());
+          setSearchParams(currentParams);
+        }}
       />
     </section>
   );
 };
-
-function udpatedNestedReplies(
-  comment: CommentWithReplies,
-  commentId: string,
-  newReplies: CommentWithReplies[],
-  pageReply: number,
-  totalReplyPages: number,
-  replyCount: number
-) {
-  const temp: CommentWithReplies[] = [];
-
-  const formatComment = {
-    ...cloneDeep(comment),
-    replies: comment.replies ? comment.replies : [],
-  };
-
-  for (const obj of formatComment.replies) {
-    if (obj.id === commentId) {
-      obj.replies = uniqBy(
-        [...(obj?.replies ?? []), ...newReplies].sort(
-          (a, b) =>
-            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-        ),
-        'id'
-      );
-      obj.replyCount = replyCount;
-      obj.pageReply = pageReply;
-      obj.totalReplyPages = totalReplyPages;
-      temp.push({ ...obj });
-    } else {
-      const updatedReplies = udpatedNestedReplies(
-        obj,
-        commentId,
-        newReplies,
-        pageReply,
-        totalReplyPages,
-        replyCount
-      );
-      temp.push({ ...obj, replies: updatedReplies });
-    }
-  }
-
-  return temp;
-}
-
-function updateNestedRepliesLikes(
-  comment: CommentWithReplies,
-  commentId: string,
-  likes: number
-) {
-  const temp: CommentWithReplies[] = [];
-
-  const formatComment = {
-    ...cloneDeep(comment),
-    replies: comment.replies ? comment.replies : [],
-  };
-
-  for (const obj of formatComment.replies) {
-    if (obj.id === commentId) {
-      obj.likes += likes;
-      temp.push({ ...obj });
-    } else {
-      const updatedReplies = updateNestedRepliesLikes(obj, commentId, likes);
-      temp.push({ ...obj, replies: updatedReplies });
-    }
-  }
-
-  return temp;
-}
