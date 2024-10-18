d526f83d6b7046583ff282ac5ca61674f7a0de8e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-18 13:35:53 +0200

    frontend: add loaders and display information when there is no data

--- frontend/src/pages/grid-articles/GridArticles.tsx
+++ frontend/src/pages/grid-articles/GridArticles.tsx
@@ -1,102 +1,98 @@
+import { Backdrop, Loader, NoDataMessage } from '../../components/shared';
 import { Card, LocalData } from '../../components/pages';
+import { faNewspaper } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { loaderArticles } from '../../api';
-import { uniqBy } from 'lodash';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { useFetchOnScroll, useScrollToggle } from '../../hooks';
+import { useCallback, useRef, useState } from 'react';
+import { useFetchOnScroll, useLoadGridArticles } from '../../hooks';
 import { UseOutletContext } from '../../types/global';
 import './GridArticles.css';
-import {
-  getCurrentCategory,
-  getFormatedData,
-  getLocalData,
-  setLocalData,
-  updateLocalData,
-} from '../../helpers';
 import {
   useLoaderData,
   useOutletContext,
   useSearchParams,
   useParams,
   Params,
+  useNavigation,
 } from 'react-router-dom';
 
 export const GridArticles = () => {
   const { category } = useParams() as Params;
   const [searchParams, setSearchParams] = useSearchParams();
-  const [state, setState] = useState<LocalData['listArticles']>([]);
+  const [state, setState] = useState<Record<string, LocalData['listArticles']>>(
+    {}
+  );
   const context = useOutletContext<UseOutletContext>();
   const firstChildRef = useRef<HTMLDivElement | null>(null);
+  const navigation = useNavigation();
 
   const data = useLoaderData() as Awaited<
     ReturnType<ReturnType<typeof loaderArticles>>
   >;
 
-  useScrollToggle({
-    onChangeVisible: (value) => {
-      if (!context.headerRef.current) return;
-      if (!context.tabsListContainerRef.current) return;
-
-      if (value) {
-        context.headerRef.current.classList.remove('slide');
-        context.tabsListContainerRef.current.classList.remove('slide');
-      } else {
-        context.headerRef.current.classList.add('slide');
-        context.tabsListContainerRef.current.classList.add('slide');
-      }
-    },
-    target: firstChildRef,
-    threshold: [0.5, 1.0],
-  });
-
   useFetchOnScroll({
     onChangeVisible: useCallback(
       (value) => {
-        if (!state.length) return;
+        if (!value) return;
+
         const currentPage = Number(searchParams.get('page')) || 1;
         if (data.response.pages === currentPage) return;
 
-        if (value) {
-          const nextPage = currentPage + 1;
-          setSearchParams({ page: `${nextPage}` }, { replace: true });
-        }
+        const nextPage = currentPage + 1;
+        setSearchParams({ page: `${nextPage}` }, { replace: true });
       },
-      [data.response.pages, setSearchParams, searchParams, state]
+      [data.response.pages, setSearchParams, searchParams]
     ),
-    target: context.footerRef,
   });
 
-  useEffect(() => {
-    if (!category) return;
-    const currentCategory = getCurrentCategory(category ?? '');
-    if (!currentCategory) return;
-    const formatedData = data.response.results.map((i) => getFormatedData(i));
+  useLoadGridArticles({
+    category,
+    data,
+    searchParams,
+    onSetState: useCallback(
+      (articles) => {
+        if (!category) return;
 
-    setState((prev) =>
-      uniqBy([...prev, ...currentCategory.listArticles, ...formatedData], 'id')
-    );
-
-    const localData = getLocalData();
-
-    const updateData = updateLocalData(
-      localData,
-      category,
-      formatedData,
-      searchParams
-    );
-
-    setLocalData(updateData);
-  }, [category, data, searchParams]);
+        setState((prev) => {
+          return {
+            ...prev,
+            [category]: articles,
+          };
+        });
+      },
+      [category]
+    ),
+  });
 
   return (
     <div className="grid-articles">
-      {state.map((article, index) => (
-        <Card
-          key={article.id}
-          handleAddSubArticle={context.handleAddSubArticle}
-          article={article}
-          ref={index === 0 ? firstChildRef : null}
-        />
-      ))}
+      {navigation.state === 'idle' ? (
+        category && state[category] ? (
+          state[category].length ? (
+            state[category].map((article, index) => (
+              <Card
+                key={article.id}
+                handleAddSubArticle={context.handleAddSubArticle}
+                article={article}
+                ref={index === 0 ? firstChildRef : null}
+              />
+            ))
+          ) : (
+            <NoDataMessage className="articles">
+              <FontAwesomeIcon icon={faNewspaper} />
+              <p>No articles</p>
+            </NoDataMessage>
+          )
+        ) : (
+          <Backdrop>
+            <Loader className="fixed" />
+          </Backdrop>
+        )
+      ) : (
+        <Backdrop>
+          <Loader className="fixed" />
+        </Backdrop>
+      )}
     </div>
   );
 };
