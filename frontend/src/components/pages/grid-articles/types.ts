d6436ee32702b149b8a2ca22bd4198b9b2e87a2d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-18 13:23:28 +0200

    frontend: update types for grid articles

--- frontend/src/components/pages/grid-articles/types.ts
+++ frontend/src/components/pages/grid-articles/types.ts
@@ -2,9 +2,5 @@ import { LocalData } from '../dashboard';
 
 export type CardProps = {
   article: LocalData['listArticles'][0];
-  handleAddSubArticle: (value: {
-    id: string;
-    title: string;
-    scroll: number;
-  }) => void;
+  handleAddSubArticle: (value: { id: string; title: string }) => void;
 };
