c846ca067f7b1b39b278ecd48b8057b7670c7abe
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-18 13:20:42 +0200

    frontend: add no data info

--- frontend/src/components/pages/dashboard/grid-layout/GridLayout.tsx
+++ frontend/src/components/pages/dashboard/grid-layout/GridLayout.tsx
@@ -1,7 +1,7 @@
 import { GridCard } from './GridCard';
 import { GridLayoutProps } from './types';
+import { NoDataMessage } from '../../../shared';
 import { Responsive, WidthProvider } from 'react-grid-layout';
-import { useNavigate } from 'react-router-dom';
 import { useRef, useState } from 'react';
 import './GridLayout.css';
 import 'react-grid-layout/css/styles.css';
@@ -16,8 +16,11 @@ import type ReactGridLayout from 'react-grid-layout';
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
-export const GridLayout = ({ layout, setLayout }: GridLayoutProps) => {
-  const navigate = useNavigate();
+export const GridLayout = ({
+  layout,
+  setLayout,
+  onNavigate,
+}: GridLayoutProps) => {
   const gridCardRef = useRef<HTMLDivElement>(null);
   const [currentBreakPoint, setCurrentBreakPoint] = useState('lg');
 
@@ -44,52 +47,58 @@ export const GridLayout = ({ layout, setLayout }: GridLayoutProps) => {
   });
 
   return (
-    <ResponsiveGridLayout
-      breakpoints={{
-        ...breakpoints,
-      }}
-      className="grid-layout"
-      cols={{ ...colsLayouts }}
-      draggableHandle=".grid-card__handle"
-      isDroppable={true}
-      layouts={
-        Object.fromEntries(
-          Object.entries(layout).map((layout) => [
-            layout[0],
-            layout[1].map((i) => i.ui),
-          ])
-        ) as ReactGridLayout.Layouts
-      }
-      onDragStop={handleDropStop}
-      onResizeStop={handleResizeStop}
-      onDrop={handleDrop}
-      onBreakpointChange={handleBreakPointChange}
-      onDropDragOver={() => ({
-        w: 1,
-        h: 4,
-        minH: 4,
-        maxH: 8,
-        minW: 1,
-        maxW: 2,
-      })}
-      rowHeight={30}
-    >
-      {layout[currentBreakPoint as keyof typeof layout].map((item) => {
-        return (
-          <GridCard
-            className="grid-card"
-            gridItem={item}
-            key={item.ui.i}
-            onClick={(value) => {
-              navigate({
-                pathname: `categories/${value}/articles`,
-                search: 'page=1',
-              });
-            }}
-            ref={gridCardRef}
-          />
-        );
-      })}
-    </ResponsiveGridLayout>
+    <div className="grid-layout-container">
+      {!layout['lg'].length && (
+        <NoDataMessage>
+          <p>News dashboard is empty</p>
+        </NoDataMessage>
+      )}
+      <ResponsiveGridLayout
+        breakpoints={{
+          ...breakpoints,
+        }}
+        className={
+          !layout['lg'].length
+            ? 'grid-layout grid-layout--transparent'
+            : 'grid-layout'
+        }
+        cols={{ ...colsLayouts }}
+        draggableHandle=".grid-card__handle"
+        isDroppable={true}
+        layouts={
+          Object.fromEntries(
+            Object.entries(layout).map((layout) => [
+              layout[0],
+              layout[1].map((i) => i.ui),
+            ])
+          ) as ReactGridLayout.Layouts
+        }
+        onDragStop={handleDropStop}
+        onResizeStop={handleResizeStop}
+        onDrop={handleDrop}
+        onBreakpointChange={handleBreakPointChange}
+        onDropDragOver={() => ({
+          w: 1,
+          h: 4,
+          minH: 4,
+          maxH: 8,
+          minW: 1,
+          maxW: 2,
+        })}
+        rowHeight={30}
+      >
+        {layout[currentBreakPoint as keyof typeof layout].map((item) => {
+          return (
+            <GridCard
+              className="grid-card"
+              gridItem={item}
+              key={item.ui.i}
+              onClick={(value) => onNavigate(value, item.page)}
+              ref={gridCardRef}
+            />
+          );
+        })}
+      </ResponsiveGridLayout>
+    </div>
   );
 };
