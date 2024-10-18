1b75394678800f4577a12d4886b00b703e60f671
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-18 13:28:25 +0200

    frontend: move functions from details articles to helpers

--- frontend/src/helpers/detailsArticle.ts
+++ frontend/src/helpers/detailsArticle.ts
@@ -1,6 +1,10 @@
-import { IElements } from '../api';
+import { cloneDeep, uniqBy } from 'lodash';
+import { ArticleDetailsElements } from '../api';
+import { CommentsWithReplies } from '../components/shared';
 
-export const getDetailsArticleImageData = (elements: IElements[]) => {
+export const getDetailsArticleImageData = (
+  elements: ArticleDetailsElements[]
+) => {
   if (elements && elements.length) {
     const main = elements.sort((a, b) => a.relation.localeCompare(b.relation));
 
@@ -26,4 +30,73 @@ export const getDetailsArticleImageData = (elements: IElements[]) => {
     credit: '',
     caption: '',
   };
+};
+
+export const udpatedNestedReplies = (
+  comment: CommentsWithReplies,
+  commentId: string,
+  newReplies: CommentsWithReplies[],
+  pageReply: number,
+  totalReplyPages: number,
+  replyCount: number
+) => {
+  const temp: CommentsWithReplies[] = [];
+
+  const formatComment = {
+    ...cloneDeep(comment),
+    replies: comment.replies ? comment.replies : [],
+  };
+
+  for (const obj of formatComment.replies) {
+    if (obj.id === commentId) {
+      obj.replies = uniqBy(
+        [...(obj?.replies ?? []), ...newReplies].sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        ),
+        'id'
+      );
+      obj.replyCount = replyCount;
+      obj.pageReply = pageReply;
+      obj.totalReplyPages = totalReplyPages;
+      temp.push({ ...obj });
+    } else {
+      const updatedReplies = udpatedNestedReplies(
+        obj,
+        commentId,
+        newReplies,
+        pageReply,
+        totalReplyPages,
+        replyCount
+      );
+      temp.push({ ...obj, replies: updatedReplies });
+    }
+  }
+
+  return temp;
+};
+
+export const updateNestedRepliesLikes = (
+  comment: CommentsWithReplies,
+  commentId: string,
+  likes: number
+) => {
+  const temp: CommentsWithReplies[] = [];
+
+  const formatComment = {
+    ...cloneDeep(comment),
+    replies: comment.replies ? comment.replies : [],
+  };
+
+  for (const obj of formatComment.replies) {
+    if (obj.id === commentId) {
+      obj.likes += likes;
+      temp.push({ ...obj });
+    } else {
+      const updatedReplies = updateNestedRepliesLikes(obj, commentId, likes);
+      temp.push({ ...obj, replies: updatedReplies });
+    }
+  }
+
+  return temp;
 };
