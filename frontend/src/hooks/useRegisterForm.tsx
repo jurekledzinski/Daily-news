246d8769abcf81e4acc404710e209ef00a9f96b4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-18 13:31:23 +0200

    frontend: add to hook register mutation

--- frontend/src/hooks/useRegisterForm.tsx
+++ frontend/src/hooks/useRegisterForm.tsx
@@ -1,9 +1,10 @@
+import { APICreateResponseSuccess, URLS } from '../api';
 import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
 import {
   FormResigsterValues,
   useRegisterFormProps,
 } from '../components/pages/';
-// import { useMutation } from '@tanstack/react-query';
 
 export const useRegisterForm = ({ onSuccess }: useRegisterFormProps) => {
   const formMethods = useForm<FormResigsterValues>({
@@ -15,11 +16,33 @@ export const useRegisterForm = ({ onSuccess }: useRegisterFormProps) => {
     },
   });
 
+  const submit = useMutation<
+    APICreateResponseSuccess,
+    Error,
+    Omit<FormResigsterValues, 'confirmPassword'>
+  >({
+    mutationFn: async (data) => {
+      const response = await fetch(URLS.CREATE_USER(), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        mode: 'cors',
+        body: JSON.stringify(data),
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      onSuccess();
+      formMethods.reset();
+    },
+  });
+
   return {
     formMethods,
     onSubmit: (data: FormResigsterValues) => {
-      console.log('DATA ------------', data);
-      onSuccess();
+      delete data.confirmPassword;
+      submit.mutate(data);
     },
   };
 };
