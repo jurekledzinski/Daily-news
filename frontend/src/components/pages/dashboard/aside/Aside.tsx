a126f898f47ee527f5eaa458b22996349db57e42
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-18 13:20:10 +0200

    frontend: add no data info

--- frontend/src/components/pages/dashboard/aside/Aside.tsx
+++ frontend/src/components/pages/dashboard/aside/Aside.tsx
@@ -1,6 +1,7 @@
-import { useOutletContext } from 'react-router-dom';
 import { GridTemplateCard, LayoutData } from '../grid-layout';
 import { IDataCategories } from '../../../../api';
+import { NoDataMessage } from '../../../shared';
+import { useOutletContext } from 'react-router-dom';
 
 type AsideProps = {
   layout: LayoutData;
@@ -11,17 +12,23 @@ export const Aside = ({ layout }: AsideProps) => {
 
   return (
     <div className="aside">
-      {categories.map((section) => {
-        const card = layout.lg.find((cardItem) => cardItem.id === section.id);
+      {categories.length ? (
+        categories.map((section) => {
+          const card = layout.lg.find((cardItem) => cardItem.id === section.id);
 
-        return (
-          <GridTemplateCard
-            data={section}
-            key={section.id}
-            isDisabled={card ? true : false}
-          />
-        );
-      })}
+          return (
+            <GridTemplateCard
+              data={section}
+              key={section.id}
+              isDisabled={card ? true : false}
+            />
+          );
+        })
+      ) : (
+        <NoDataMessage className="aside-message">
+          <p>Please try later, api has limited amount of requestes per day</p>
+        </NoDataMessage>
+      )}
     </div>
   );
 };
