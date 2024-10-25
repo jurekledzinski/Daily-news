10e6eedcda1772e864c238f4409377bb143db00d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-25 15:59:50 +0200

    frontend: update article details

--- frontend/src/components/pages/details-article/types.ts
+++ frontend/src/components/pages/details-article/types.ts
@@ -1,6 +1,7 @@
 import { ArticleData, Likes } from '../../../api';
 import { CommentInput, CommentsWithReplies } from '../../shared';
 import { LegacyRef } from 'react';
+import { UserState } from '../../../store';
 
 export type ArticleDetailsProps = {
   comments: CommentsWithReplies[];
@@ -10,4 +11,5 @@ export type ArticleDetailsProps = {
   methodSubmitLike: (data: Likes) => void;
   onShowReplies: (commendId: string) => void;
   onShowMoreReplies: (commendId: string, pageReply: number) => void;
+  userData: UserState;
 };
