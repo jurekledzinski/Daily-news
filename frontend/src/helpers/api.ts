e07075353280fe85f51c5fc11d06cb7867c05097
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-25 16:05:15 +0200

    frontend: add helpers for api footer global

--- frontend/src/helpers/api.ts
+++ frontend/src/helpers/api.ts
@@ -17,3 +17,23 @@ export const refetchQueries = async (
     queryKey,
   });
 };
+
+export const tryCatch = <T, K, N = unknown>(
+  fn: (body: N) => Promise<T>
+): ((body: N) => Promise<T | K>) => {
+  return async (body: N): Promise<T | K> => {
+    try {
+      return await fn(body);
+    } catch (error) {
+      return error as K;
+    }
+  };
+};
+
+export const fetchOrCache = async <T>(
+  queryClient: QueryClient,
+  queryKey: string[],
+  queryFn: () => Promise<T>
+) => {
+  return (queryClient.getQueryData(queryKey) as T) ?? (await queryFn());
+};
--- frontend/src/helpers/global.ts
+++ frontend/src/helpers/global.ts
@@ -16,3 +16,25 @@ export const getCurrentCategory = (category: string) => {
 export const setLocalData = (data: unknown) => {
   localStorage.setItem('categories', JSON.stringify(data));
 };
+
+export const getCookie = (name: string) => {
+  const cookies = document.cookie.split(';');
+
+  const cookieValue = cookies
+    .find((row) => row.trim().startsWith(name))
+    ?.split('=')[1];
+
+  if (cookieValue) {
+    return JSON.parse(cookieValue);
+  }
+
+  return null;
+};
+
+export const removeCookie = (name: string) => {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+};
+
+export const setCookie = (name: string, data: unknown) => {
+  document.cookie = `${name}=${JSON.stringify(data)}`;
+};
