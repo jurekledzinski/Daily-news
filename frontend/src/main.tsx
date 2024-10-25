c027b50089942d9beddcea0e7e530336094506c8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-25 15:53:54 +0200

    frontend: add userProvider to store logged in user

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,18 @@ import router from './routes/AppRouter';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
+import { UserProvider } from './store';
 import './index.css';
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <RouterProvider router={router} />
+      <UserProvider>
+        <RouterProvider router={router} />
+      </UserProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
