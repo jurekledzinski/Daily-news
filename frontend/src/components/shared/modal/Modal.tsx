5d3ab01ea419399164ecfbd18bf4ff2c5655bc3b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-25 16:03:38 +0200

    frontend: update and refactor Modal component

--- frontend/src/components/shared/modal/Modal.tsx
+++ frontend/src/components/shared/modal/Modal.tsx
@@ -2,34 +2,61 @@ import { forwardRef, Ref, useImperativeHandle, useRef } from 'react';
 import { ModalProps } from './types';
 
 export const Modal = forwardRef<HTMLDialogElement, ModalProps>(
-  ({ children, title, onClose }, ref: Ref<HTMLDialogElement>) => {
+  (
+    {
+      form,
+      onClose,
+      className,
+      cancelButton = 'Cancel',
+      confirmButton = 'Confirm',
+      openButton = 'Open modal',
+      children,
+      title,
+    },
+    ref: Ref<HTMLDialogElement>
+  ) => {
     const localRef = useRef<HTMLDialogElement>(null);
 
     useImperativeHandle(ref, () => localRef.current!);
 
     return (
-      <dialog className="modal" role="dialog" ref={localRef}>
-        <header className="modal__header">
-          <h5 className="modal__title">{title}</h5>
-        </header>
-        <div className="modal__content">{children}</div>
-        <footer className="modal__footer">
-          <button className="modal__button" form="form" type="submit">
-            Submit
-          </button>
-          <button
-            autoFocus
-            className="modal__button"
-            onClick={() => {
-              if (!localRef.current) return;
-              localRef.current.close();
-              onClose && onClose();
-            }}
-          >
-            Cancel
-          </button>
-        </footer>
-      </dialog>
+      <>
+        <button
+          className="button-open-modal"
+          onClick={() => {
+            if (!localRef.current) return;
+            localRef.current.showModal();
+          }}
+        >
+          {openButton}
+        </button>
+        <dialog
+          className={['modal', className].filter(Boolean).join(' ')}
+          role="dialog"
+          ref={localRef}
+        >
+          <header className="modal__header">
+            <h5 className="modal__title">{title}</h5>
+          </header>
+          <div className="modal__body">{children}</div>
+          <footer className="modal__footer">
+            <button className="modal__button" form={form} type="submit">
+              {confirmButton}
+            </button>
+            <button
+              autoFocus
+              className="modal__button"
+              onClick={() => {
+                if (!localRef.current) return;
+                localRef.current.close();
+                onClose && onClose();
+              }}
+            >
+              {cancelButton}
+            </button>
+          </footer>
+        </dialog>
+      </>
     );
   }
 );
--- frontend/src/components/shared/modal/types.ts
+++ frontend/src/components/shared/modal/types.ts
@@ -1,5 +1,10 @@
 export type ModalProps = {
+  className?: string;
+  confirmButton?: string;
+  cancelButton?: string;
   children: React.ReactNode;
+  form: string;
   title: string;
   onClose?: () => void;
+  openButton?: string;
 };
