99138eca3695380a35ee8b356563eb6778804f91
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-25 15:53:24 +0200

    backend: add more request routes

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -1,8 +1,19 @@
 import { checkAuthentication } from '../middlewares/authorization';
-import { getUser } from '../controllers/users';
 import { Router } from 'express';
+import {
+  changeUserPassword,
+  getUser,
+  deleteUser,
+  logoutUser,
+  updateUserProfile,
+} from '../controllers/users';
+
 const router = Router();
 
 router.get('/', checkAuthentication, getUser);
+router.route('/logout').post(logoutUser);
+router.route('/update_profile/:id').patch(updateUserProfile);
+router.route('/change_password/:id').patch(changeUserPassword);
+router.route('/delete_user/:id').delete(deleteUser);
 
 export default router;
