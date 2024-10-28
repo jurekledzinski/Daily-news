6c5179464dd93799c4be5d08cd65d0e4b4d12640
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-28 08:21:49 +0100

    frontend: format urls

--- frontend/src/api/urls.ts
+++ frontend/src/api/urls.ts
@@ -1,37 +1,71 @@
 export const URLS = {
   FETCH_USER: () => 'http://localhost:5000/api/v1/users',
+
   CREATE_USER: () => 'http://localhost:5000/api/v1/register',
+
   LOGIN_USER: () => 'http://localhost:5000/api/v1/login',
+
   LOGOUT_USER: () => 'http://localhost:5000/api/v1/users/logout',
-  UPDATE_USER_PROFILE: (id: string) =>
-    `http://localhost:5000/api/v1/users/update_profile/${id}`,
-  CHANGE_USER_PASSWORD: (id: string) =>
-    `http://localhost:5000/api/v1/users/change_password/${id}`,
-  DELETE_USER_ACCOUNT: (id: string) =>
-    `http://localhost:5000/api/v1/users/delete_user/${id}`,
+
+  UPDATE_USER_PROFILE: (id: string) => {
+    const url = `http://localhost:5000/api/v1/users/update_profile/${id}`;
+
+    return url;
+  },
+
+  CHANGE_USER_PASSWORD: (id: string) => {
+    const url = `http://localhost:5000/api/v1/users/change_password/${id}`;
+
+    return url;
+  },
+
+  DELETE_USER_ACCOUNT: (id: string) => {
+    const url = `http://localhost:5000/api/v1/users/delete_user/${id}`;
+
+    return url;
+  },
+
   GET_CATEGORIES_ARTICLES: () => {
-    return `https://content.guardianapis.com/sections?&format=json&api-key=${
+    const url = `https://content.guardianapis.com/sections?&format=json&api-key=${
       import.meta.env.VITE_API_KEY
     }`;
+
+    return url;
   },
+
   GET_ARTICLES: (category: string, page: string) => {
-    return `https://content.guardianapis.com/${category}?show-fields=body,headline,trailText&show-elements=image&page=${page}&page-size=12&api-key=${
+    const url = `https://content.guardianapis.com/${category}?show-fields=body,headline,trailText&show-elements=image&page=${page}&page-size=12&api-key=${
       import.meta.env.VITE_API_KEY
     }`;
+
+    return url;
   },
+
   GET_DETAILS_ARTICLE: (id: string) => {
-    return `https://content.guardianapis.com/${id}?show-fields=body,trailText&show-elements=image&api-key=${
+    const url = `https://content.guardianapis.com/${id}?show-fields=body,trailText&show-elements=image&api-key=${
       import.meta.env.VITE_API_KEY
     }`;
+
+    return url;
   },
+
   GET_COMMENTS: (id: string, page: string) => {
-    return `http://localhost:5000/api/v1/comments/${id}?page=${page}`;
+    const url = `http://localhost:5000/api/v1/comments/${id}?page=${page}`;
+
+    return url;
   },
+
   GET_COMMENT_REPLIES: (id: string, commentId: string, page: string) => {
-    return `http://localhost:5000/api/v1/comments/${id}/${commentId}?page_reply=${page}`;
+    const url = `http://localhost:5000/api/v1/comments/${id}/${commentId}?page_reply=${page}`;
+
+    return url;
   },
+
   CREATE_COMMENT: () => 'http://localhost:5000/api/v1/comments/create',
+
   UPDATE_COMMENT_LIKE: (id: string, commentId: string) => {
-    return `http://localhost:5000/api/v1/comments/likes/${id}/${commentId}`;
+    const url = `http://localhost:5000/api/v1/comments/likes/${id}/${commentId}`;
+
+    return url;
   },
 };
