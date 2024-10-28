49af575bed60b5f97af3ed80a0cd9338789259f3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-28 11:50:43 +0100

    frontend: Pass status of comments to display fetch error in case

--- frontend/src/components/pages/details-article/types.ts
+++ frontend/src/components/pages/details-article/types.ts
@@ -1,15 +1,15 @@
 import { ArticleData, Likes } from '../../../api';
 import { CommentInput, CommentsWithReplies } from '../../shared';
-import { LegacyRef } from 'react';
 import { UserState } from '../../../store';
 
 export type ArticleDetailsProps = {
   comments: CommentsWithReplies[];
   data: ArticleData;
-  headerRef: LegacyRef<HTMLDivElement> | undefined;
   methodSubmitComment: (data: CommentInput, commentId?: string) => void;
   methodSubmitLike: (data: Likes) => void;
   onShowReplies: (commendId: string) => void;
   onShowMoreReplies: (commendId: string, pageReply: number) => void;
+  successComments: boolean;
+  successRepliesComments: boolean;
   userData: UserState;
 };
