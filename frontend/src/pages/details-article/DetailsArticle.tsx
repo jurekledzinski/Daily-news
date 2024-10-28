6ef9a291a0ab578d0209bff8fc1a065399eb30df
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-28 11:53:24 +0100

    frontend: Remove header ref from details article

--- frontend/src/pages/details-article/DetailsArticle.tsx
+++ frontend/src/pages/details-article/DetailsArticle.tsx
@@ -1,8 +1,10 @@
 import { ArticleDetails } from '../../components/pages';
 import { cloneDeep } from 'lodash';
-import { CommentsWithReplies } from '../../components/shared';
+import { CommentsWithReplies, NoDataMessage } from '../../components/shared';
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { loaderDetailsArticle } from '../../api';
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useUserStore } from '../../store';
 import {
   getDetailsArticleImageData,
@@ -24,7 +26,6 @@ import {
 type LoaderData = Awaited<ReturnType<ReturnType<typeof loaderDetailsArticle>>>;
 
 export const DetailsArticle = () => {
-  const articleHeaderRef = useRef<HTMLDivElement | null>(null);
   const [stateComments, setStateComments] = useState<
     Record<string, CommentsWithReplies[]>
   >({});
@@ -85,11 +86,25 @@ export const DetailsArticle = () => {
     setStateComments,
   });
 
+  if (
+    data.detailsArticle.response.status === 'error' &&
+    'message' in data.detailsArticle.response
+  ) {
+    return (
+      <NoDataMessage className="articles server-error">
+        <FontAwesomeIcon icon={faTriangleExclamation} />
+        <p>
+          {data.detailsArticle.response.message ??
+            "Couldn't fetch data, please try later"}
+        </p>
+      </NoDataMessage>
+    );
+  }
+
   return (
     <section className="section section--details-article">
       <ArticleDetails
         comments={id && stateComments[id] ? stateComments[id] : []}
-        headerRef={articleHeaderRef}
         data={{
           id: article.id,
           sectionId: article.sectionId,
@@ -113,6 +128,8 @@ export const DetailsArticle = () => {
           currentParams.set('page_reply', (page + 1).toString());
           setSearchParams(currentParams);
         }}
+        successComments={data.comments.success}
+        successRepliesComments={data.commentReplies.success}
         userData={state}
       />
     </section>
