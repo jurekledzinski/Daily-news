9ce506146a811ef3761e8e8394b386be1091800d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-28 11:50:07 +0100

    frontend: Remove ref from header

--- frontend/src/components/pages/details-article/Header.tsx
+++ frontend/src/components/pages/details-article/Header.tsx
@@ -1,17 +1,13 @@
-import { forwardRef, Ref } from 'react';
-
 type ArticleHeaderProps = {
   title: string;
   trailText: string;
 };
 
-export const Header = forwardRef<HTMLDivElement, ArticleHeaderProps>(
-  ({ title, trailText }, ref: Ref<HTMLDivElement>) => {
-    return (
-      <header className="details-article__header" ref={ref}>
-        <h1 className="details-article__title">{title}</h1>
-        <p className="details-article__trail">{trailText}</p>
-      </header>
-    );
-  }
-);
+export const Header = ({ title, trailText }: ArticleHeaderProps) => {
+  return (
+    <header className="details-article__header">
+      <h1 className="details-article__title">{title}</h1>
+      <p className="details-article__trail">{trailText}</p>
+    </header>
+  );
+};
