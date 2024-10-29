af4d00e46f6ecb2cbbddf9db5b67c2f134f64206
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-29 17:43:42 +0100

    backend: check authentification on comments and user operations

--- backend/routes/comments.ts
+++ backend/routes/comments.ts
@@ -7,9 +7,11 @@ import {
   updateCommentLikes,
 } from '../controllers/comments';
 
+import { checkAuthentication } from '../middlewares/authorization';
+
 router.route('/:article_id').get(getComments);
 router.route('/:article_id/:comment_id').get(getCommentReplies);
-router.route('/create').post(createComment);
+router.route('/create').post(checkAuthentication, createComment);
 router.route('/likes/:article_id/:comment_id').patch(updateCommentLikes);
 
 export default router;
--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -11,9 +11,14 @@ import {
 const router = Router();
 
 router.get('/', checkAuthentication, getUser);
-router.route('/logout').post(logoutUser);
-router.route('/update_profile/:id').patch(updateUserProfile);
-router.route('/change_password/:id').patch(changeUserPassword);
-router.route('/delete_user/:id').delete(deleteUser);
+router.route('/logout').post(checkAuthentication, logoutUser);
+router
+  .route('/update_profile/:id')
+  .patch(checkAuthentication, updateUserProfile);
+router
+  .route('/change_password/:id')
+  .patch(checkAuthentication, changeUserPassword);
+
+router.route('/delete_user/:id').delete(checkAuthentication, deleteUser);
 
 export default router;
