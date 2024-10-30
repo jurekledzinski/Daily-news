9d47602df7c21f3acd3b9a883734a985151fbefa
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-30 14:28:51 +0100

    frontend: pass action data hook react router to parent component

--- frontend/src/components/pages/details-article/types.ts
+++ frontend/src/components/pages/details-article/types.ts
@@ -1,8 +1,10 @@
 import { ArticleData, Likes } from '../../../api';
 import { CommentInput, CommentsWithReplies } from '../../shared';
 import { UserState } from '../../../store';
+import { ActionData } from '../../../types';
 
 export type ArticleDetailsProps = {
+  actionData: ActionData;
   comments: CommentsWithReplies[];
   data: ArticleData;
   methodSubmitComment: (data: CommentInput, commentId?: string) => void;
