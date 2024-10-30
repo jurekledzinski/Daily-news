821a469ea2efcb9bec7be4598941a3afb6327887
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-10-30 14:26:02 +0100

    backend: remove comments in csrf controller

--- backend/controllers/csrfController.ts
+++ backend/controllers/csrfController.ts
@@ -2,27 +2,8 @@ import { csrfSync } from 'csrf-sync';
 import { Request, Response } from 'express';
 import { STATUS_CODE } from '../constants';
 
-const { generateToken, storeTokenInState } = csrfSync();
+const { generateToken } = csrfSync();
 
 export const csrfController = (req: Request, res: Response) => {
-  //   console.log('CSRF token ---- 1', req.session);
-  //   if (!req.session.csrfToken) {
-  //     req.session.csrfToken = generateToken(req);
-  //   } else {
-  //     // Regenerate token
-  //     req.session.csrfToken = generateToken(req);
-  //   }
-
-  //   console.log('CSRF token ---- 2', req.session);
-
-  //   return res.json({ token: req.session.csrfToken });
-
-  //   if (!req.session.csrfToken) {
-  //     req.session.csrfToken = generateToken(req);
-  //   }
-
-  //   console.log('kkkk', req.session.id);
-  console.log('--- csrf session before send', req.session);
-
   return res.status(STATUS_CODE.OK).json({ token: generateToken(req, true) });
 };
