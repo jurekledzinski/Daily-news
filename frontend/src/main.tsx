41e1b30bd9bf8f9d940455c1979b2797f1fc2843
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-03 11:52:36 +0100

    frontend: add ToastContainer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,16 @@ import router from './routes/AppRouter';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { UserProvider } from './store';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ToastContainer position="top-right" theme="light" />
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <UserProvider>
