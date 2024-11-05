249bf8218252317fe763666413e6edcb15a17f09
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-05 15:33:29 +0100

    frontend: update detect mobile helper

--- frontend/src/helpers/gridTemplateCard.ts
+++ frontend/src/helpers/gridTemplateCard.ts
@@ -1,8 +1,11 @@
 export const detectMobile = () => {
+  console.log('navigator.userAgent', navigator.userAgent);
   if (
     navigator.userAgent.match(/iPhone/i) ||
     navigator.userAgent.match(/iPad/i) ||
-    navigator.userAgent.match(/Android/i)
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/Macintosh/i) ||
+    navigator.userAgent.match(/Windows NT/i)
   ) {
     return true;
   }
