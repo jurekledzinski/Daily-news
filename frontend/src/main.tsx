80a50ab0d911e5682d556e93b8b65ee65846116e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-06 17:00:01 +0100

    frontend: remove react query dev tools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import router from '@routes/AppRouter';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { UserProvider } from '@store/index';
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ToastContainer position="top-right" theme="light" autoClose={1500} />
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       <UserProvider>
         <RouterProvider router={router} />
       </UserProvider>
