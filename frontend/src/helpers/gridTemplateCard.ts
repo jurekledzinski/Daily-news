ad6125ab8e43d39e2cae65ea81f29111adde4597
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-07 11:44:15 +0100

    frontend: get back to previous detect mobile helper

--- frontend/src/helpers/gridTemplateCard.ts
+++ frontend/src/helpers/gridTemplateCard.ts
@@ -2,9 +2,7 @@ export const detectMobile = () => {
   if (
     navigator.userAgent.match(/iPhone/i) ||
     navigator.userAgent.match(/iPad/i) ||
-    navigator.userAgent.match(/Android/i) ||
-    navigator.userAgent.match(/Macintosh/i) ||
-    navigator.userAgent.match(/Windows NT/i)
+    navigator.userAgent.match(/Android/i)
   ) {
     return true;
   }
