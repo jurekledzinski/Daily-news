0f27bd8d05432801ef773de7302cc32f6d7ab003
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-07 14:17:37 +0100

    backend: change file express-session.d.ts

--- backend/types/express-session.d.ts
+++ backend/types/express-session.d.ts
@@ -1,11 +1,3 @@
 import { Session } from 'express-session';
 
 declare module 'express-session';
-
-declare global {
-  namespace Express {
-    interface Request {
-      session: Session;
-    }
-  }
-}
