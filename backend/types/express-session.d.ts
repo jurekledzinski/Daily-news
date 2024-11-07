7362285b6b54269ca62e05077570cffbc1c796a3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-07 15:37:16 +0100

    backend: update express session.d.ts

--- backend/types/express-session.d.ts
+++ backend/types/express-session.d.ts
@@ -1,13 +1,9 @@
-import type { CookieOptions } from 'express-session';
+import type { Session } from 'express-session';
 
 declare global {
   namespace Express {
     interface Request {
-      session: {
-        id?: string;
-        email?: string;
-        cookie: CookieOptions;
-      };
+      session: Session;
     }
   }
 }
