7ed536f94742906230b9cc32523d4ee55240cf1a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-24 11:16:17 +0100

    Change imports images

--- frontend/src/images/categories.ts
+++ frontend/src/images/categories.ts
@@ -1,46 +1,24 @@
-const artanddesign =
-  'https://ik.imagekit.io/mdklwracd5rti/News/artanddesign_Y8B_QB0DQ.jpg?updatedAt=1730480113193';
-const australia_news =
-  'https://ik.imagekit.io/mdklwracd5rti/News/australia-news_j6zhKTWz6.jpg?updatedAt=1730480113873';
-const cities =
-  'https://ik.imagekit.io/mdklwracd5rti/News/cities_ERI-PWYXa.jpg?updatedAt=1730480114142';
-const environment =
-  'https://ik.imagekit.io/mdklwracd5rti/News/environment_WglCWMmnr.jpg?updatedAt=1730480111604';
-const fashion =
-  'https://ik.imagekit.io/mdklwracd5rti/News/fashion_jwd6waKT0x.jpg?updatedAt=1730480111560';
-const film =
-  'https://ik.imagekit.io/mdklwracd5rti/News/film_Zb4er0brP.jpg?updatedAt=1730480113914';
-const food =
-  'https://ik.imagekit.io/mdklwracd5rti/News/food_pnr34b2bO.jpg?updatedAt=1730480113448';
-const football =
-  'https://ik.imagekit.io/mdklwracd5rti/News/football_x4TCaKlfK.jpg?updatedAt=1730480113756';
-const games =
-  'https://ik.imagekit.io/mdklwracd5rti/News/games_jc8A2CzJ1.jpg?updatedAt=1730480113456';
-const music =
-  'https://ik.imagekit.io/mdklwracd5rti/News/music_fKOggqxPO.jpg?updatedAt=1730480111341';
-const news =
-  'https://ik.imagekit.io/mdklwracd5rti/News/news_ommdfC8Big.jpg?updatedAt=1730480111446';
-const politics =
-  'https://ik.imagekit.io/mdklwracd5rti/News/politics_F7WKlTh5n.jpg?updatedAt=1730480111532';
-const science =
-  'https://ik.imagekit.io/mdklwracd5rti/News/science__1kD32LnL.jpg?updatedAt=1730480111476';
-const sport =
-  'https://ik.imagekit.io/mdklwracd5rti/News/sport_91luyMYzq.jpg?updatedAt=1730480111470';
-const technology =
-  'https://ik.imagekit.io/mdklwracd5rti/News/technology_Ju0FmXtv7g.jpg?updatedAt=1730480111594';
-const travel =
-  'https://ik.imagekit.io/mdklwracd5rti/News/travel_t_r2tUbVqj.jpg?updatedAt=1730480111736';
-const uk_news =
-  'https://ik.imagekit.io/mdklwracd5rti/News/uk-news_dUyFJz0YQr.jpg?updatedAt=1730480111467';
-const us_news =
-  'https://ik.imagekit.io/mdklwracd5rti/News/us-news_3LZwuHh_v.jpg?updatedAt=1730483655537';
-const weather =
-  'https://ik.imagekit.io/mdklwracd5rti/News/weather_Vq51vn12k.jpg?updatedAt=1730480112840';
-const world =
-  'https://ik.imagekit.io/mdklwracd5rti/News/world_MuMv6mnyGx.jpg?updatedAt=1730480112914';
+const australia_news = '/images/australia-news.jpg';
+const cities = '/images/cities.jpg';
+const environment = '/images/environment.jpg';
+const fashion = '/images/fashion.jpg';
+const film = '/images/film.jpg';
+const food = '/images/food.jpg';
+const football = '/images/football.jpg';
+const games = '/images/games.jpg';
+const music = '/images/music.jpg';
+const news = '/images/music.jpg';
+const politics = '/images/music.jpg';
+const science = '/images/music.jpg';
+const sport = '/images/music.jpg';
+const technology = '/images/music.jpg';
+const travel = '/images/music.jpg';
+const uk_news = '/images/music.jpg';
+const us_news = '/images/music.jpg';
+const weather = '/images/music.jpg';
+const world = '/images/music.jpg';
 
 export const images = [
-  artanddesign,
   australia_news,
   cities,
   environment,
