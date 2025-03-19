833de1dd835a2a73cf034a5e1bfdf02f99df3e6b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-20 00:24:59 +0100

    Create types file used in details artilce components

---
+++ frontend/src/pages/details-article/types.ts
@@ -0,0 +1,33 @@
+import { ActionData } from '@/types';
+import { CommentsWithReplies } from '@/components/shared';
+import { NavigateOptions, URLSearchParamsInit } from 'react-router-dom';
+import { UseQueryResult } from '@tanstack/react-query';
+import {
+  APIGuardianResDetailsSuccess,
+  APIResPagination,
+  ArticleDetails,
+  Comment,
+  loaderDetailsArticle,
+} from '@/api';
+
+export type StateComments = Record<string, CommentsWithReplies[]>;
+
+export type LoaderData = Awaited<
+  ReturnType<ReturnType<typeof loaderDetailsArticle>>
+>;
+
+export type DetailsArticleProps = {
+  articleId: string;
+  actionData: ActionData;
+  comments: UseQueryResult<APIResPagination<Comment[]>, Error>;
+  commentsReplies: UseQueryResult<APIResPagination<Comment[]>, Error>;
+  loaderData: APIGuardianResDetailsSuccess<ArticleDetails> | null;
+  token: string;
+  onSetSearchParams: (
+    value: URLSearchParams | URLSearchParamsInit,
+    options?: NavigateOptions
+  ) => void;
+  onShowReplies: (commendId: string) => void;
+  onShowMoreReplies: (commendId: string, pageReply: number) => void;
+  searchParams: URLSearchParams;
+};
