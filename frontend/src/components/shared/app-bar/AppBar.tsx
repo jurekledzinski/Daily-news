d0f297eaaecbb547dc0a8e7066faa97674eebc1a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-25 15:11:54 +0100

    Add AppBar component to shared components

---
+++ frontend/src/components/shared/app-bar/AppBar.tsx
@@ -0,0 +1,7 @@
+import styles from './AppBar.module.css';
+import { AppBarProps } from './types';
+import { classNames } from '@helpers';
+
+export const AppBar = ({ children, className }: AppBarProps) => {
+  return <header className={classNames(styles.header, className ?? '')}>{children}</header>;
+};
