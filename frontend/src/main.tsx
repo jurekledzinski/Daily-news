bc035d4bbd0cca26b088c5cf95f901b7ccd01794
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-25 14:58:13 +0100

    Update main.tsx change toast library to react  hot toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import router from '@/routes/AppRouter';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RouterProvider } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { RouterProvider } from 'react-router';
+import { Toaster } from 'react-hot-toast';
 import { UserProvider } from '@/store';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
+import './styles/main.css';
 
 export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ToastContainer position="top-right" theme="light" autoClose={1500} />
+    <Toaster position="top-left" />
     <QueryClientProvider client={queryClient}>
       <UserProvider>
         <RouterProvider router={router} />
