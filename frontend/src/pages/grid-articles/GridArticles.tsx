3dc3c976bc31da4c0d2faf8887c95635513c726b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-25 15:40:00 +0100

    Create new GridArticles component

--- frontend/src/pages/grid-articles/GridArticles.tsx
+++ frontend/src/pages/grid-articles/GridArticles.tsx
@@ -1,107 +1,39 @@
-import { Card, LocalData } from '@/components/pages';
-import { checkIsExistArticle, useLoadGridArticles } from './hooks';
-import { faNewspaper } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { getCurrentCategory } from '@/helpers';
-import { loaderArticles } from '@/api';
-import { NoDataMessage } from '@/components/shared';
-import { useCallback, useRef, useState } from 'react';
-import { useFetchOnScroll } from '@/hooks';
-import { UseOutletContext } from '@/types';
-import './GridArticles.css';
-import {
-  useLoaderData,
-  useOutletContext,
-  useSearchParams,
-  useParams,
-  Params,
-} from 'react-router-dom';
+import cardStyles from '../../components/shared/card/Card.module.css';
+import styles from './GridArticles.module.css';
+import { Button, Card, CardContent, CardFooter, CardHeader, Heading } from '@components/shared';
+import { SearchResponse } from '@guardian/content-api-models/v1/SearchResponse';
+import { useLoaderData } from 'react-router';
 
 export const GridArticles = () => {
-  const { category } = useParams() as Params;
-  const currentCategory = getCurrentCategory(category ?? '');
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [state, setState] = useState<Record<string, LocalData['listArticles']>>(
-    {}
-  );
+  const articles = useLoaderData<SearchResponse>();
 
-  const context = useOutletContext<UseOutletContext>();
-  const firstChildRef = useRef<HTMLDivElement | null>(null);
-
-  const data = useLoaderData() as Awaited<
-    ReturnType<ReturnType<typeof loaderArticles>>
-  >;
-
-  useFetchOnScroll({
-    onChangeVisible: useCallback(
-      (value) => {
-        if (!data || !value) return;
-
-        const currentPage = Number(searchParams.get('page')) || 1;
-        if (data.response.pages === currentPage) return;
-
-        const nextPage = currentPage + 1;
-        setSearchParams({ page: `${nextPage}` }, { replace: true });
-      },
-      [data, setSearchParams, searchParams]
-    ),
-  });
-
-  useLoadGridArticles({
-    category,
-    data,
-    searchParams,
-    onSetState: useCallback(
-      (articles) => {
-        if (!category) return;
-
-        const isExist = checkIsExistArticle(state[category], articles);
-        if (!isExist) return;
-
-        setState((prev) => {
-          return {
-            ...prev,
-            [category]: articles,
-          };
-        });
-      },
-      [category, state]
-    ),
-  });
-
-  if (!data) {
-    return (
-      <NoDataMessage className="articles">
-        <FontAwesomeIcon icon={faNewspaper} />
-        <p>No articles</p>
-      </NoDataMessage>
-    );
-  }
+  //   Dodaj  authora zdjęcia text title w content
+  //   Intersection observer by gdy znikną tabs to pojawiły sie płynie podczas skrolowania w dół
+  //   czasem error gdy szybko przełączasz taby
 
   return (
-    <div
-      className={
-        currentCategory && currentCategory.articles.length
-          ? 'grid-articles grid-articles__sub-articles'
-          : 'grid-articles'
-      }
-    >
-      {category && state[category] && state[category].length
-        ? state[category].map((article, index) => (
-            <Card
-              key={article.id}
-              handleAddSubArticle={context.handleAddSubArticle}
-              article={article}
-              ref={index === 0 ? firstChildRef : null}
-            />
-          ))
-        : category &&
-          state[category] && (
-            <NoDataMessage className="articles">
-              <FontAwesomeIcon icon={faNewspaper} />
-              <p>No articles</p>
-            </NoDataMessage>
-          )}
+    <div className={styles.grid}>
+      {articles.results.map((article) => {
+        const imageUrl = article.elements?.length ? article.elements[0].assets[1].file : '';
+        return (
+          <Card key={article.id} className={cardStyles.card}>
+            <CardHeader
+              className={cardStyles.header}
+              style={{ backgroundImage: `url(${imageUrl})` }}
+            >
+              <span className={styles.photograph}>Joe Doe</span>
+            </CardHeader>
+            <CardContent className={cardStyles.content}>
+              <Heading className={styles.heading} level={6}>
+                {article.webTitle}
+              </Heading>
+            </CardContent>
+            <CardFooter className={cardStyles.footer}>
+              <Button className={styles.button} color="info" label="Read more ..." size="size-xs" />
+            </CardFooter>
+          </Card>
+        );
+      })}
     </div>
   );
 };
