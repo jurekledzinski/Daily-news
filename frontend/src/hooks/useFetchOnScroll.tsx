1ccb7089c631b96e18f3057d0998c60d318395ea
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-25 15:33:07 +0100

    Leave useFetchOnScroll hook

--- frontend/src/hooks/useFetchOnScroll.tsx
+++ frontend/src/hooks/useFetchOnScroll.tsx
@@ -4,15 +4,12 @@ type useFetchOnScrollProps = {
   onChangeVisible: (value: boolean) => void;
 };
 
-export const useFetchOnScroll = ({
-  onChangeVisible,
-}: useFetchOnScrollProps) => {
+export const useFetchOnScroll = ({ onChangeVisible }: useFetchOnScrollProps) => {
   useEffect(() => {
     const onScroll = () => {
       if (
         window.scrollY !== 0 &&
-        Math.ceil(window.innerHeight + window.scrollY) >=
-          document.body.scrollHeight
+        Math.ceil(window.innerHeight + window.scrollY) >= document.body.scrollHeight
       ) {
         onChangeVisible(true);
       }
