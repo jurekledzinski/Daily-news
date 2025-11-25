99567d4c00924eb5bdba2db1d347c6485364bb08
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-25 21:57:12 +0100

    Update GridArticles component add useFetchOnScroll hook and EmptyState component

--- frontend/src/pages/grid-articles/GridArticles.tsx
+++ frontend/src/pages/grid-articles/GridArticles.tsx
@@ -1,39 +1,32 @@
-import cardStyles from '../../components/shared/card/Card.module.css';
 import styles from './GridArticles.module.css';
-import { Button, Card, CardContent, CardFooter, CardHeader, Heading } from '@components/shared';
+import { CardArticle } from '@components/pages';
+import { EmptyState } from '@components/shared';
 import { SearchResponse } from '@guardian/content-api-models/v1/SearchResponse';
-import { useLoaderData } from 'react-router';
+import { useFetchOnScroll } from '@hooks';
+import { useLoaderData, useSearchParams } from 'react-router';
 
 export const GridArticles = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const articles = useLoaderData<SearchResponse>();
 
-  //   Dodaj  authora zdjęcia text title w content
-  //   Intersection observer by gdy znikną tabs to pojawiły sie płynie podczas skrolowania w dół
+  useFetchOnScroll({
+    onReachBottom: () => {
+      const page = Number(searchParams.get('page') ?? '1');
+      setSearchParams({ page: String(page + 1) });
+    },
+  });
+
   //   czasem error gdy szybko przełączasz taby
+
+  if (!articles?.results?.length) {
+    return <EmptyState text="No articles found." src="/images/api-limit.png" />;
+  }
 
   return (
     <div className={styles.grid}>
-      {articles.results.map((article) => {
-        const imageUrl = article.elements?.length ? article.elements[0].assets[1].file : '';
-        return (
-          <Card key={article.id} className={cardStyles.card}>
-            <CardHeader
-              className={cardStyles.header}
-              style={{ backgroundImage: `url(${imageUrl})` }}
-            >
-              <span className={styles.photograph}>Joe Doe</span>
-            </CardHeader>
-            <CardContent className={cardStyles.content}>
-              <Heading className={styles.heading} level={6}>
-                {article.webTitle}
-              </Heading>
-            </CardContent>
-            <CardFooter className={cardStyles.footer}>
-              <Button className={styles.button} color="info" label="Read more ..." size="size-xs" />
-            </CardFooter>
-          </Card>
-        );
-      })}
+      {(articles?.results ?? []).map((article) => (
+        <CardArticle article={article} key={article.id} />
+      ))}
     </div>
   );
 };
