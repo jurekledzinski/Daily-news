58db0ef241c9b9e2cfe63e7b69a4d2bf5eb0eb7b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-25 21:50:35 +0100

    Add className prop to extend current styles in TabsList component

--- frontend/src/components/shared/tabs/components/tabs-list/TabsList.tsx
+++ frontend/src/components/shared/tabs/components/tabs-list/TabsList.tsx
@@ -1,10 +1,19 @@
-import { TabsListProps } from './types';
 import styles from './TabsList.module.css';
+import { classNames } from '@helpers';
+import { forwardRef } from 'react';
+import { TabsListProps } from './types';
 
-export const TabsList = ({ children, ...props }: TabsListProps) => {
-  return (
-    <div {...props} className={styles.tabList} role="tablist">
-      {children}
-    </div>
-  );
-};
+export const TabsList = forwardRef<HTMLDivElement, TabsListProps>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <div
+        {...props}
+        className={classNames(styles.tabList, className ?? '')}
+        ref={ref}
+        role="tablist"
+      >
+        {children}
+      </div>
+    );
+  }
+);
