54a036c08a7fdf97c9052b80331408ad15960bbe
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-25 15:05:41 +0100

    Delete articles details header errorpage card components used in details and grid articles

--- frontend/src/components/pages/grid-articles/card/types.ts
+++
@@ -1,6 +0,0 @@
-import { LocalData } from '../../dashboard';
-
-export type CardProps = {
-  article: LocalData['listArticles'][0];
-  handleAddSubArticle: (value: { id: string; title: string }) => void;
-};
