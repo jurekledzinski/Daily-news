2643346a78bf87849719eee221df033344dc00d8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-25 15:28:27 +0100

    Add classNames helpers

---
+++ frontend/src/helpers/classNames.ts
@@ -0,0 +1,15 @@
+export const classNames = (...classes: (string | false | null | undefined)[]) => {
+  const value = classes.filter(Boolean).join(' ');
+  return value === '' ? undefined : value;
+};
+
+export const generateClassNames = (
+  styles: Record<string, string>,
+  modifiers: Record<string, boolean>
+) => {
+  return Object.entries(modifiers)
+    .filter((item) => item[1])
+    .map(([key]) => styles[key])
+    .filter(Boolean)
+    .join(' ');
+};
