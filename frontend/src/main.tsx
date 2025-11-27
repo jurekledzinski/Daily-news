a4765eae4a987493e690db1598ca53f33a6879ac
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-27 16:57:56 +0100

    Update main.tsx file add PersistQueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import router from '@/routes/AppRouter';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import router, { asyncStoragePersister, queryClient } from '@routes/AppRouter';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { RouterProvider } from 'react-router';
 import { Toaster } from 'react-hot-toast';
-import { UserProvider } from '@/store';
+import { UserProvider } from '@store';
 import './styles/main.css';
-
-export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Toaster position="top-left" />
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister: asyncStoragePersister }}
+    >
       <UserProvider>
         <RouterProvider router={router} />
       </UserProvider>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </React.StrictMode>
 );
