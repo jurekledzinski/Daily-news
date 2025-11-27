e29f6a009b62e4e388416a2890dd8f415e859cbe
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-27 16:59:53 +0100

    Update import routes in article details page loader

--- frontend/src/api/loaders/articleDetailsPage.ts
+++ frontend/src/api/loaders/articleDetailsPage.ts
@@ -1,6 +1,6 @@
 import { APIErrorResponse, APISuccessResponse } from '../api';
 import { fetchApi } from '../api-calls';
-import { queryClient } from '@/routes';
+import { queryClient } from '@routes';
 import { URLS } from '../urls';
 import type { LoaderFunction } from 'react-router';
 import type { Content } from '@guardian/content-api-models/v1/content';
@@ -10,7 +10,7 @@ export const loaderArticleDetailsPage: LoaderFunction = async ({ params }) => {
   const id = decodeURIComponent(params.id ?? '');
 
   try {
-    const data = await queryClient.fetchQuery<APISuccessResponse<Content>, APIErrorResponse>({
+    const data = await queryClient.ensureQueryData<APISuccessResponse<Content>, APIErrorResponse>({
       queryKey: ['details-article', category, id],
       queryFn: async () => {
         const response = await fetchApi({ url: URLS.GET_DETAILS_ARTICLE(id) });
