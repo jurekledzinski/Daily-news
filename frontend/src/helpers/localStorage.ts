3c8d741f6b5be70ca32ca40696137d87e59d5458
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-27 17:07:46 +0100

    Update helpers for localstorage

--- frontend/src/helpers/localStorage.ts
+++ frontend/src/helpers/localStorage.ts
@@ -1,13 +1,18 @@
 import { GridStackNode } from 'gridstack';
 
-export const getLocalData = <T>(name: string) => {
-  const tempData = localStorage.getItem(name) ?? '[]';
-  const localData: T[] = JSON.parse(tempData);
+export const getLocalData = <T>(name: string, type: 'array' | 'string' | 'object' = 'array') => {
+  const constructor = { array: '[]', string: '""', object: '{}' }[type];
+  const tempData = localStorage.getItem(name) ?? constructor;
+  const localData: T = JSON.parse(tempData);
   return localData;
 };
 
 export const getCurrentCategory = <T extends GridStackNode>(category: string) => {
-  return getLocalData<T>('categories').find((i) => i.id === category);
+  return getLocalData<T[]>('categories').find((i) => i.id === category);
+};
+
+export const removeLocalData = (key: string) => {
+  localStorage.removeItem(key);
 };
 
 export const setLocalData = <T>(name: string, data: T) => {
