14978a141026282bf8f3d733480885145b6ebe36
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-27 17:15:45 +0100

    Set up asyncStoragePersister in AppRouter component

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -4,7 +4,9 @@ import DetailsArticle from '@/pages/details-article';
 import GridArticles from '@/pages/grid-articles';
 import Home from '@/pages/home';
 import Profile from '@/pages/profile';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
 import { createBrowserRouter } from 'react-router';
+import { del, get, set } from 'idb-keyval';
 import { ErrorPage } from '@components/pages';
 import { ProtectedRoute } from '@components/shared';
 import { QueryClient } from '@tanstack/react-query';
@@ -13,11 +15,25 @@ import {
   actionHome,
   actionProfileUser,
   loaderArticleDetailsPage,
-  loaderArticlesListPage,
   loaderHomePage,
 } from '@api';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { gcTime: Infinity, refetchOnWindowFocus: false, staleTime: 1000 * 60 * 60 * 24 },
+  },
+});
+
+export const asyncStoragePersister = createAsyncStoragePersister({
+  storage: {
+    getItem: async (key: string) => {
+      const item = await get(key);
+      return item ? JSON.stringify(item) : null;
+    },
+    removeItem: async (key: string) => await del(key),
+    setItem: async (key: string, value: string) => set(key, JSON.parse(value)),
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -41,7 +57,6 @@ const router = createBrowserRouter([
             index: true,
             element: <GridArticles />,
             errorElement: <ErrorPage />,
-            loader: loaderArticlesListPage,
           },
           {
             path: 'article/:id',
