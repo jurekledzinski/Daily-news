6fc7f2cb8a68a37a0949c177900ae482e75d7dd0
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-27 17:08:07 +0100

    Update reexport index file in global hooks

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,4 +1,7 @@
 export * from './click-outside';
 export * from './fetch-on-scroll';
+export * from './infinite-query-fetch';
+export * from './page-search-params';
+export * from './persisted-scroll-page';
 export * from './window-resize';
 export * from './window-scroll';
