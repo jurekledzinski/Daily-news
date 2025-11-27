2613950874923847273250d40d85a485280a0cab
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-27 17:02:13 +0100

    Update CardArticle add heading styles, trailer text and photograph credits

--- frontend/src/components/pages/grid-articles/card-article/CardArticle.tsx
+++ frontend/src/components/pages/grid-articles/card-article/CardArticle.tsx
@@ -2,19 +2,23 @@ import cardStyles from '../../../shared/card/Card.module.css';
 import styles from './CardArticle.module.css';
 import { Button, Card, CardContent, CardFooter, CardHeader, Heading } from '@components/shared';
 import { CardArticleProps } from './types';
+import { htmlToText } from 'html-to-text';
 
 export const CardArticle = ({ article }: CardArticleProps) => {
-  const imageUrl = article.elements?.length ? article.elements[0].assets[1].file : '';
+  const { elements, fields } = article;
+  const imageUrl = elements?.length ? elements[0].assets[1].file : '';
+  const imageCredit = elements?.length ? elements[0].assets[1].typeData?.credit : '';
 
   return (
     <Card className={cardStyles.card}>
       <CardHeader className={cardStyles.header} style={{ backgroundImage: `url(${imageUrl})` }}>
-        <span className={styles.photograph}>Photo: John Doe</span>
+        {imageCredit && <span className={styles.photograph}>{imageCredit}</span>}
       </CardHeader>
       <CardContent className={cardStyles.content}>
         <Heading className={styles.heading} level={6}>
-          {article.webTitle}
+          {fields?.headline ?? ''}
         </Heading>
+        <p className={styles.text}>{htmlToText(fields?.trailText ?? '')}</p>
       </CardContent>
       <CardFooter className={cardStyles.footer}>
         <Button className={styles.button} color="info" label="Read more ..." size="size-xs" />
