a58c7b4466799b3522a76464b79d4acff0a513f6
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-29 11:45:26 +0100

    Refactor DetailsArticle page and add Article component to it

--- frontend/src/pages/details-article/DetailsArticle.tsx
+++ frontend/src/pages/details-article/DetailsArticle.tsx
@@ -1,40 +1,20 @@
+import { Article } from '@components/pages';
 import { Asset } from '@guardian/content-api-models/v1/asset';
-import { Container, Heading, ImageWithLoader } from '@components/shared';
-import { formatDateLocalString, optionsFormatDate1 } from '@helpers';
-import { htmlToText } from 'html-to-text';
+import { Container, EmptyState } from '@components/shared';
 import { useLoaderData } from 'react-router';
-import './DetailsArticle.css';
+import '@components/pages/details-article/article/Article.css';
 import type { Content } from '@guardian/content-api-models/v1/content';
 
 export const DetailsArticle = () => {
-  const article = useLoaderData<{ data: Content & { image: Asset } }>();
+  const article = useLoaderData<{ data: Content & { image: Asset }; success: boolean }>();
 
-  const data = formatDateLocalString({
-    date: article.data.webPublicationDate ? String(article.data.webPublicationDate) : new Date(),
-    options: optionsFormatDate1,
-  });
+  if (!article.success) {
+    return <EmptyState text="No article found." src="/info/article.png" />;
+  }
 
   return (
     <Container className="container-details">
-      <Heading className="headling mt-md" level={4}>
-        {article.data.webTitle}
-      </Heading>
-
-      <i>{htmlToText(article.data.fields?.trailText ?? '')}</i>
-
-      <span className="datePublish">Publish date: {data}</span>
-
-      {article.data.image.file && (
-        <div className="container-image">
-          <ImageWithLoader src={article.data.image.file} />
-
-          {article.data.image.typeData?.photographer && (
-            <span className="caption">{article.data.image.typeData?.credit}</span>
-          )}
-        </div>
-      )}
-
-      <div dangerouslySetInnerHTML={{ __html: article.data.fields?.body ?? '' }}></div>
+      <Article article={article.data} />
     </Container>
   );
 };
