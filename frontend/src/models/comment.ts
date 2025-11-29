248a9a12d910796eaf04b6b4a4ffd6329fb89eb4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-29 15:58:06 +0100

    Update comment zod schema model

--- frontend/src/models/comment.ts
+++ frontend/src/models/comment.ts
@@ -3,10 +3,8 @@ import * as z from 'zod';
 const CommentSchema = z.object({
   createdAt: z.string(),
   id: z.string(),
-  idArticle: z.string(),
-  likes: z.string(),
-  parentCommentId: z.union([z.string(), z.null()]),
-  replyCount: z.number(),
+  articleId: z.string(),
+  likes: z.number(),
   text: z.string(),
   user: z.string(),
   userId: z.string(),
@@ -14,23 +12,3 @@ const CommentSchema = z.object({
 });
 
 export type Comment = z.infer<typeof CommentSchema>;
-
-// Na chwile dla potrzeb refaktoru
-
-const CommentAndReplies = CommentSchema.extend({
-  pageReply: z.number(),
-  totalReplyPages: z.number(),
-  get replies() {
-    return z.array(CommentAndReplies);
-  },
-});
-
-export type CommentAndReplies = z.infer<typeof CommentAndReplies>;
-
-const Likes = z.object({
-  commentId: z.string(),
-  likes: z.number(),
-  parentCommentId: z.string(),
-});
-
-export type Likes = z.infer<typeof Likes>;
