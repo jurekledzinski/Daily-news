a2f3dd25db05830ef83503555b00275993df5837
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-29 17:28:42 +0100

    Show info alert when user not login in comments section in details article

--- frontend/src/pages/details-article/DetailsArticle.tsx
+++ frontend/src/pages/details-article/DetailsArticle.tsx
@@ -1,15 +1,18 @@
 import styles from './DetailsArticle.module.css';
+import { Alert, AlertIcon, AlertMessage, Comment, Container, EmptyState } from '@components/shared';
 import { Article, CommentForm, CommentsSection, useComment } from '@components/pages';
 import { Asset } from '@guardian/content-api-models/v1/asset';
-import { Comment, Container, EmptyState } from '@components/shared';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { formatDateLocalString, optionsFormatDate1 } from '@helpers';
 import { useLoaderData } from 'react-router';
 import '@components/pages/details-article/article/Article.css';
 import type { Content } from '@guardian/content-api-models/v1/content';
 
+const userId = '';
+
 export const DetailsArticle = () => {
   const article = useLoaderData<{ data: Content & { image: Asset }; success: boolean }>();
-  const form = useComment({ articleId: '', onSuccess: () => {}, status: 'idle', userId: '' });
+  const form = useComment({ articleId: '', onSuccess: () => {}, status: 'idle', userId });
 
   const createAt = formatDateLocalString({
     date: new Date(),
@@ -23,21 +26,29 @@ export const DetailsArticle = () => {
   return (
     <Container className={styles.container}>
       <Article article={article.data} />
-      <CommentsSection>
-        <CommentForm
-          controls={form.methods}
-          isPending={false}
-          onSubmit={form.onSubmit}
-        ></CommentForm>
-        <Comment
-          comment={{
-            createdAt: createAt,
-            likes: 123,
-            text: 'Finally, someone covering this in a balanced way. Well done!',
-            user: 'Joe Doe',
-          }}
-        />
-      </CommentsSection>
+      {Boolean(!userId) && (
+        <Alert fullWidth color="info" variant="outlined">
+          <AlertIcon icon={faLock} color="info" />
+          <AlertMessage message="Sign in to add comments" />
+        </Alert>
+      )}
+      {Boolean(userId) && (
+        <CommentsSection>
+          <CommentForm
+            controls={form.methods}
+            isPending={false}
+            onSubmit={form.onSubmit}
+          ></CommentForm>
+          <Comment
+            comment={{
+              createdAt: createAt,
+              likes: 123,
+              text: 'Finally, someone covering this in a balanced way. Well done!',
+              user: 'Joe Doe',
+            }}
+          />
+        </CommentsSection>
+      )}
     </Container>
   );
 };
