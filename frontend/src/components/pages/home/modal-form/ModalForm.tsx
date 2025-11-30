0efe8835538b55f3b0e4a5d324331b8e263caf45
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-11-30 14:34:44 +0100

    Refactor ModalForm component used in header

--- frontend/src/components/pages/home/modal-form/ModalForm.tsx
+++ frontend/src/components/pages/home/modal-form/ModalForm.tsx
@@ -1,48 +1,35 @@
-import { Button, Heading, Modal, ModalLayout } from '@/components/shared';
+import { Heading, Modal, ModalLayout } from '@components/shared';
 import { ModalFormProps } from './types';
 
 export const ModalForm = ({
   children,
   form,
-  icon,
   label,
   open,
   onClose,
-  onOpen,
   subTitle,
   title,
 }: ModalFormProps) => {
   return (
-    <>
-      <Button
-        className="r-xs"
+    <Modal open={open} portal>
+      <ModalLayout
+        confirmText={label}
         color="info"
-        type="button"
-        iconEnd={[icon]}
-        label={label}
-        onClick={onOpen}
-        size="size-xs"
-      />
-      <Modal open={open} portal>
-        <ModalLayout
-          confirmText={label}
-          color="info"
-          form={form}
-          title={label}
-          variant="contained"
-          onCancel={onClose}
-          onClose={onClose}
-          type="submit"
-        >
-          <Heading className="mb-xs" level={5}>
-            {title}
-          </Heading>
-          <p className="mb-md" style={{ fontSize: 12 }}>
-            {subTitle}
-          </p>
-          {children}
-        </ModalLayout>
-      </Modal>
-    </>
+        form={form}
+        onCancel={onClose}
+        onClose={onClose}
+        title={label}
+        type="submit"
+        variant="contained"
+      >
+        <Heading className="mb-xs" level={5}>
+          {title}
+        </Heading>
+        <p className="mb-md" style={{ fontSize: 12 }}>
+          {subTitle}
+        </p>
+        {children}
+      </ModalLayout>
+    </Modal>
   );
 };
--- frontend/src/components/pages/home/modal-form/types.ts
+++ frontend/src/components/pages/home/modal-form/types.ts
@@ -1,11 +1,7 @@
-import { Icon } from '@types';
-
 export type ModalFormProps = {
   form: string;
-  icon: Icon;
   label: string;
   onClose: () => void;
-  onOpen: () => void;
   open: boolean;
   subTitle: string;
   title: string;
