87a4a6556b929c215c71908a90a7778d8ae26966
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-02 02:15:47 +0100

    Backend: Update requestLogout helper function

--- backend/helpers/users.ts
+++ backend/helpers/users.ts
@@ -1,28 +1,18 @@
 import { config } from '../config';
-import { CustomError } from '../error';
 import { NextFunction, Request, Response } from 'express';
-import { STATUS_CODE } from '../constants';
+import { STATUS_CODE, STATUS_MESSAGE } from '../constants';
+import { throwError } from '../error';
 
-export const requestLogout = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const requestLogout = (req: Request, res: Response, next: NextFunction) => {
   req.logout((err: Error) => {
     if (err) return next(err);
     if (!req.session) {
-      throw new CustomError('SERVER ERROR', STATUS_CODE.NOT_FOUND);
+      throwError(STATUS_MESSAGE[STATUS_CODE.NOT_FOUND], STATUS_CODE.NOT_FOUND);
     }
 
     req.session.destroy((err: Error) => {
       if (err) return next(err);
 
-      res.clearCookie('tsge', {
-        path: '/',
-        httpOnly: false,
-        secure: config.node_env === 'production',
-        sameSite: config.node_env === 'production' ? 'none' : 'strict',
-      });
       res.clearCookie('bmg-seqdk', {
         path: '/',
         httpOnly: true,
