aaee8adc0a630d7d9f835377c2297a45eb15950c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-02 02:11:52 +0100

    Backend: Update in comments controller handle errors

--- backend/controllers/comments.ts
+++ backend/controllers/comments.ts
@@ -1,102 +1,74 @@
 import xss from 'xss';
+import { buildResponse, calculateSkipCount, commentAggergation } from '../helpers';
 import { CommentSchema, IComment, LikesSchema } from '../models';
-import { CustomError } from '../error';
 import { getCollectionDb } from '../config';
 import { ObjectId } from 'mongodb';
-import { PAGE_SIZE, STATUS_CODE } from '../constants';
+import { PAGE_SIZE, STATUS_CODE, STATUS_MESSAGE } from '../constants';
 import { Request, Response } from 'express';
+import { throwError } from '../error';
 import { transformDocument } from '../helpers';
-import { tryCatch } from '../helpers';
-import {
-  buildResponse,
-  calculateSkipCount,
-  commentAggergation,
-} from '../helpers';
 
 const collection = getCollectionDb<IComment>('comments');
 
-export const getComments = tryCatch<IComment[]>(
-  async (req: Request, res: Response) => {
-    if (!collection) {
-      throw new CustomError(
-        'Internal server error',
-        STATUS_CODE.INTERNAL_SERVER_ERROR
-      );
-    }
-
-    const idArticle = decodeURIComponent(req.params.article_id);
-    const page = req.query.page ?? '1';
-    const skipCount = calculateSkipCount(page);
-
-    const total = await collection.countDocuments({
-      idArticle,
-      parentCommentId: null,
-    });
-
-    const result = await commentAggergation(
-      idArticle,
-      null,
-      skipCount,
-      PAGE_SIZE
-    );
-
-    const formatResults = transformDocument<IComment>(result);
-
-    const totalPages = Math.ceil(total / PAGE_SIZE);
-
-    return res.status(STATUS_CODE.OK).json({
-      ...buildResponse(formatResults, page, totalPages),
-    });
+export const getComments = async (req: Request, res: Response) => {
+  if (!collection) {
+    throwError(STATUS_MESSAGE[STATUS_CODE.INTERNAL_ERROR], STATUS_CODE.INTERNAL_ERROR);
   }
-);
 
-export const getCommentReplies = tryCatch<IComment[]>(
-  async (req: Request, res: Response) => {
-    if (!collection) {
-      throw new CustomError(
-        'Internal server error',
-        STATUS_CODE.INTERNAL_SERVER_ERROR
-      );
-    }
+  const idArticle = decodeURIComponent(req.params.article_id);
+  const page = (req.query.page ?? '1') as string;
+  const skipCount = calculateSkipCount(page);
 
-    const idArticle = decodeURIComponent(req.params.article_id);
-    const comment_id = req.params.comment_id;
-    const page = req.query.page_reply ?? '1';
-    const skipCount = calculateSkipCount(page);
+  const total = await collection.countDocuments({
+    idArticle,
+    parentCommentId: null,
+  });
 
-    const total = await collection.countDocuments({
-      idArticle,
-      parentCommentId: comment_id,
-    });
+  const result = await commentAggergation(idArticle, null, skipCount, PAGE_SIZE);
 
-    const result = await commentAggergation(
-      idArticle,
-      comment_id,
-      skipCount,
-      PAGE_SIZE
-    );
+  const formatResults = transformDocument<IComment>(result);
 
-    const formatResults = transformDocument<IComment>(result);
+  const totalPages = Math.ceil(total / PAGE_SIZE);
 
-    const totalPages = Math.ceil(total / PAGE_SIZE);
+  return res.status(STATUS_CODE.OK).json({
+    ...buildResponse(formatResults, page, totalPages),
+  });
+};
 
-    return res.status(STATUS_CODE.OK).json({
-      ...buildResponse(formatResults, page, totalPages),
-      replyCount: total,
-    });
+export const getCommentReplies = async (req: Request, res: Response) => {
+  if (!collection) {
+    throwError(STATUS_MESSAGE[STATUS_CODE.INTERNAL_ERROR], STATUS_CODE.INTERNAL_ERROR);
   }
-);
 
-export const createComment = tryCatch(async (req: Request, res: Response) => {
+  const idArticle = decodeURIComponent(req.params.article_id);
+  const comment_id = req.params.comment_id;
+  const page = (req.query.page_reply ?? '1') as string;
+  const skipCount = calculateSkipCount(page);
+
+  const total = await collection.countDocuments({
+    idArticle,
+    parentCommentId: comment_id,
+  });
+
+  const result = await commentAggergation(idArticle, comment_id, skipCount, PAGE_SIZE);
+
+  const formatResults = transformDocument<IComment>(result);
+
+  const totalPages = Math.ceil(total / PAGE_SIZE);
+
+  return res.status(STATUS_CODE.OK).json({
+    ...buildResponse(formatResults, page, totalPages),
+    replyCount: total,
+  });
+};
+
+export const createComment = async (req: Request, res: Response) => {
   const parsedData = CommentSchema.parse(req.body);
 
   const parentId = req.body.parentCommentId === 'null';
 
   if (!collection) {
-    throw new CustomError(
-      'Internal server error',
-      STATUS_CODE.INTERNAL_SERVER_ERROR
-    );
+    throwError(STATUS_MESSAGE[STATUS_CODE.INTERNAL_ERROR], STATUS_CODE.INTERNAL_ERROR);
   }
 
   const likes = xss(parsedData.likes.toString());
@@ -112,32 +84,27 @@ export const createComment = tryCatch(async (req: Request, res: Response) => {
   });
 
   return res.status(STATUS_CODE.OK).json({ success: true });
-});
+};
 
-export const updateCommentLikes = tryCatch(
-  async (req: Request, res: Response) => {
-    const parsedData = LikesSchema.parse(req.body);
+export const updateCommentLikes = async (req: Request, res: Response) => {
+  const parsedData = LikesSchema.parse(req.body);
 
-    if (!collection) {
-      throw new CustomError(
-        'Internal server error',
-        STATUS_CODE.INTERNAL_SERVER_ERROR
-      );
-    }
-
-    const articleId = decodeURIComponent(req.params.article_id);
-    const commentId = req.params.comment_id;
-
-    const likes = xss(parsedData.likes.toString());
-
-    await collection.updateOne(
-      {
-        _id: new ObjectId(commentId),
-        idArticle: articleId,
-      },
-      { $inc: { likes: parseInt(likes) } }
-    );
-
-    return res.status(STATUS_CODE.OK).json({ success: true });
+  if (!collection) {
+    throwError(STATUS_MESSAGE[STATUS_CODE.INTERNAL_ERROR], STATUS_CODE.INTERNAL_ERROR);
   }
-);
+
+  const articleId = decodeURIComponent(req.params.article_id);
+  const commentId = req.params.comment_id;
+
+  const likes = xss(parsedData.likes.toString());
+
+  await collection.updateOne(
+    {
+      _id: new ObjectId(commentId),
+      idArticle: articleId,
+    },
+    { $inc: { likes: parseInt(likes) } }
+  );
+
+  return res.status(STATUS_CODE.OK).json({ success: true });
+};
