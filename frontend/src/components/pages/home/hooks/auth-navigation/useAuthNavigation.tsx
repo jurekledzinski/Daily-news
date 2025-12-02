c3e463562dc3afa622afe0f889dc9273c0e441ef
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-02 02:26:29 +0100

    Frontend: Create useAuthNavigation during authentication

--- frontend/src/components/pages/home/hooks/auth-navigation/useAuthNavigation.tsx
+++ frontend/src/components/pages/home/hooks/auth-navigation/useAuthNavigation.tsx
@@ -1,13 +1,17 @@
 import { showSuccessToast } from '@helpers';
 import { useMatch, useNavigate } from 'react-router';
+import { useUserStore } from '@/store';
 
 export const useAuthNavigation = () => {
   const navigate = useNavigate();
   const matchProfile = useMatch('/profile/:id');
+  const { dispatch } = useUserStore();
 
   const navigateLogout = () => {
+    dispatch({ type: 'LOGOUT_USER' });
     showSuccessToast('Logout successful');
     if (matchProfile) navigate('/', { replace: true });
+    else navigate(window.location.pathname, { replace: true });
   };
 
   const navigateProfile = () => navigate(`profile/${'userId'}`);
