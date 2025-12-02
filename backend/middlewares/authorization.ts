8727ab99c2482e0db55620bbf63a20a8a3ddb8fb
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-02 02:16:12 +0100

    Backend: Update checkAuthentication helper function

--- backend/middlewares/authorization.ts
+++ backend/middlewares/authorization.ts
@@ -1,15 +1,11 @@
-import { CustomError } from '../error';
 import { NextFunction, Request, Response } from 'express';
-import { STATUS_CODE } from '../constants';
+import { STATUS_CODE, STATUS_MESSAGE } from '../constants';
+import { throwError } from '../error';
 
-export const checkAuthentication = (
-  req: Request,
-  _: Response,
-  next: NextFunction
-) => {
+export const checkAuthentication = (req: Request, _: Response, next: NextFunction) => {
   if (req.isAuthenticated()) {
     return next();
   }
 
-  throw new CustomError('Not authenticated', STATUS_CODE.UNAUTHORIZED);
+  throwError(STATUS_MESSAGE[STATUS_CODE.UNAUTHORIZED], STATUS_CODE.UNAUTHORIZED);
 };
