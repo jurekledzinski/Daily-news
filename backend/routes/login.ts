c24523083be640917c5d6c7178a43709ad39ddda
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-02 02:17:51 +0100

    Backend: Update login route imports

--- backend/routes/login.ts
+++ backend/routes/login.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { loginRateLimiter } from '../middlewares';
-import { loginUser } from '../controllers/login';
+import { loginUser } from '../controllers';
 const router = express.Router();
 
 router.route('/').post(loginRateLimiter, loginUser);
