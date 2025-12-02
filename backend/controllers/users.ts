3eea7bdbc05422490a1fee496f49452cc93d6ef9
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-02 02:12:44 +0100

    Backend: Update in user controller handle errors

--- backend/controllers/users.ts
+++ backend/controllers/users.ts
@@ -1,128 +1,81 @@
 import bcrypt from 'bcrypt';
 import xss from 'xss';
-import { CustomError } from '../error';
+import { ChangeUserPasswordSchema, UpdateUserProfileSchema, UserData } from '../models';
 import { getCollectionDb } from '../config';
 import { NextFunction, Request, Response } from 'express';
 import { ObjectId } from 'mongodb';
-import { requestLogout, responseCookie } from '../helpers';
-import { STATUS_CODE } from '../constants';
-import { tryCatch } from '../helpers';
-import {
-  ChangeUserPasswordSchema,
-  UpdateUserProfileSchema,
-  UserData,
-} from '../models';
+import { requestLogout } from '../helpers';
+import { STATUS_CODE, STATUS_MESSAGE } from '../constants';
+import { throwError } from '../error';
 
 const collection = getCollectionDb<UserData>('users');
 
-export const getUser = tryCatch<UserData[]>(
-  async (req: Request, res: Response) => {
-    if (!collection) {
-      throw new CustomError(
-        'Internal server error',
-        STATUS_CODE.INTERNAL_SERVER_ERROR
-      );
-    }
+export const getUser = async (req: Request, res: Response) => {
+  return res.status(STATUS_CODE.OK).json({ success: true, payload: { result: req.user } });
+};
 
-    const maxAge = req.session?.cookie.maxAge!;
-
-    responseCookie(res, maxAge);
-
-    return res
-      .status(STATUS_CODE.OK)
-      .json({ success: true, payload: { result: req.user } });
-  }
-);
-
-export const logoutUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (!collection) {
-    throw new CustomError(
-      'Internal server error',
-      STATUS_CODE.INTERNAL_SERVER_ERROR
-    );
-  }
-
+export const logoutUser = async (req: Request, res: Response, next: NextFunction) => {
   requestLogout(req, res, next);
 };
 
-export const updateUserProfile = tryCatch<UserData[]>(
-  async (req: Request, res: Response) => {
-    if (!collection) {
-      throw new CustomError(
-        'Internal server error',
-        STATUS_CODE.INTERNAL_SERVER_ERROR
-      );
-    }
-
-    const parsedData = UpdateUserProfileSchema.parse(req.body);
-
-    const sessionUser = req.user! as UserData;
-
-    const isExist =
-      (await collection.findOne({ email: req.body.email })) !== null;
-
-    if (isExist && sessionUser && sessionUser.email !== req.body.email) {
-      throw new CustomError('User exist', STATUS_CODE.CONFLICT);
-    }
-
-    await collection.updateOne(
-      { _id: new ObjectId(req.params.id) },
-      { $set: { email: xss(parsedData.email), name: xss(parsedData.name) } }
-    );
-
-    return res.status(STATUS_CODE.OK).json({ success: true });
+export const updateUserProfile = async (req: Request, res: Response) => {
+  if (!collection) {
+    throwError(STATUS_MESSAGE[STATUS_CODE.INTERNAL_ERROR], STATUS_CODE.INTERNAL_ERROR);
   }
-);
 
-export const changeUserPassword = tryCatch<UserData[]>(
-  async (req: Request, res: Response) => {
-    if (!collection) {
-      throw new CustomError(
-        'Internal server error',
-        STATUS_CODE.INTERNAL_SERVER_ERROR
-      );
-    }
+  const parsedData = UpdateUserProfileSchema.parse(req.body);
 
-    const parsedData = ChangeUserPasswordSchema.parse(req.body);
+  const sessionUser = req.user! as UserData;
 
-    const salt = 10;
-    const password = xss(parsedData.password);
+  const isExist = (await collection.findOne({ email: req.body.email })) !== null;
 
-    const generatedSalt = await bcrypt.genSalt(salt);
-    const hashedPassword = await bcrypt.hash(password, generatedSalt);
-
-    await collection.updateOne(
-      { _id: new ObjectId(req.params.id) },
-      { $set: { password: hashedPassword } }
-    );
-
-    return res.status(STATUS_CODE.OK).json({ success: true });
+  if (isExist && sessionUser && sessionUser.email !== req.body.email) {
+    throwError('User exist', STATUS_CODE.CONFLICT);
   }
-);
 
-export const deleteUser = tryCatch<UserData[]>(
-  async (req: Request, res: Response, next: NextFunction) => {
-    if (!collection) {
-      throw new CustomError(
-        'Internal server error',
-        STATUS_CODE.INTERNAL_SERVER_ERROR
-      );
-    }
+  await collection.updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: { email: xss(parsedData.email), name: xss(parsedData.name) } }
+  );
 
-    const id = req.params.id;
+  return res.status(STATUS_CODE.OK).json({ success: true });
+};
 
-    const result = await collection.deleteOne({
-      _id: new ObjectId(id),
-    });
-
-    if (result.deletedCount) {
-      requestLogout(req, res, next);
-    } else {
-      res.status(STATUS_CODE.OK).json({ success: false });
-    }
+export const changeUserPassword = async (req: Request, res: Response) => {
+  if (!collection) {
+    throwError(STATUS_MESSAGE[STATUS_CODE.INTERNAL_ERROR], STATUS_CODE.INTERNAL_ERROR);
   }
-);
+
+  const parsedData = ChangeUserPasswordSchema.parse(req.body);
+
+  const salt = 10;
+  const password = xss(parsedData.password);
+
+  const generatedSalt = await bcrypt.genSalt(salt);
+  const hashedPassword = await bcrypt.hash(password, generatedSalt);
+
+  await collection.updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: { password: hashedPassword } }
+  );
+
+  return res.status(STATUS_CODE.OK).json({ success: true });
+};
+
+export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  if (!collection) {
+    throwError(STATUS_MESSAGE[STATUS_CODE.INTERNAL_ERROR], STATUS_CODE.INTERNAL_ERROR);
+  }
+
+  const id = req.params.id;
+
+  const result = await collection.deleteOne({
+    _id: new ObjectId(id),
+  });
+
+  if (result.deletedCount) {
+    requestLogout(req, res, next);
+  } else {
+    res.status(STATUS_CODE.OK).json({ success: false });
+  }
+};
