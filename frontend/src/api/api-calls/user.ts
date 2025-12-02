b15f925e0b32ba74960b521c93c3c49baa050f04
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-02 02:23:00 +0100

    Frontend: update user api calls for actions

--- frontend/src/api/api-calls/user.ts
+++ frontend/src/api/api-calls/user.ts
@@ -1,44 +1,40 @@
 import { APIErrorResponse, APISuccessResponse } from '../api';
-import { User } from '../types';
 import { fetchApi } from './helpers';
-import { tryCatch } from '@/helpers';
+import { tryCatch } from '@helpers';
 import { URLS } from '../urls';
+import { User } from '@models';
 
-export const registerUser = tryCatch<
-  Omit<APISuccessResponse<unknown>, 'payload'>,
-  APIErrorResponse,
-  User
->(async (body: User) => {
-  return await fetchApi({
-    url: URLS.CREATE_USER(),
-    method: 'POST',
-    mode: 'cors',
-    credentials: 'include',
-    body: JSON.stringify(body),
-  });
-});
+type APISuccessWithoutPayload = Omit<APISuccessResponse, 'payload'>;
 
-export const loginUser = tryCatch<
-  Omit<APISuccessResponse<unknown>, 'payload'>,
-  APIErrorResponse,
-  Omit<User, 'id' | 'name'>
->(async (body: Omit<User, 'id' | 'name'>) => {
-  return await fetchApi({
-    url: URLS.LOGIN_USER(),
-    method: 'POST',
-    mode: 'cors',
-    credentials: 'include',
-    body: JSON.stringify(body),
-  });
-});
-
-export const logoutUser = tryCatch<Omit<APISuccessResponse<unknown>, 'payload'>, APIErrorResponse>(
-  async () => {
+export const registerUser = tryCatch<APISuccessWithoutPayload, APIErrorResponse, User>(
+  async (body: User) => {
     return await fetchApi({
-      url: URLS.LOGOUT_USER(),
+      url: URLS.CREATE_USER(),
       method: 'POST',
       mode: 'cors',
       credentials: 'include',
+      body: JSON.stringify(body),
     });
   }
 );
+
+export const loginUser = tryCatch<APISuccessResponse, APIErrorResponse, Omit<User, 'id' | 'name'>>(
+  async (body: Omit<User, 'id' | 'name'>) => {
+    return await fetchApi({
+      url: URLS.LOGIN_USER(),
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      body: JSON.stringify(body),
+    });
+  }
+);
+
+export const logoutUser = tryCatch<APISuccessWithoutPayload, APIErrorResponse>(async () => {
+  return await fetchApi({
+    url: URLS.LOGOUT_USER(),
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'include',
+  });
+});
