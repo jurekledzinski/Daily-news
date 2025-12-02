91274d165bc95878e7312d002e5520ecf9789e67
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-02 18:01:32 +0100

    Frontend: Add useFetchUserData too fetch logged in user

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -2,8 +2,11 @@ import styles from './Home.module.css';
 import { Footer, Header } from '@components/pages';
 import { Outlet, ScrollRestoration } from 'react-router';
 import { Suspense } from 'react';
+import { useFetchUserData } from '@hooks';
 
 export const Home = () => {
+  useFetchUserData();
+
   return (
     <div className={styles.container}>
       <Header />
