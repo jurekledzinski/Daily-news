15075a8dafb5b7059e6b9abce51c5afb1b373a14
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-02 02:12:16 +0100

    Backend: Update in login controller handle errors

--- backend/controllers/login.ts
+++ backend/controllers/login.ts
@@ -1,59 +1,41 @@
-import { CustomError } from '../error';
+import { CustomError, throwError } from '../error';
 import { NextFunction, Request, Response } from 'express';
-import { passport } from '../middlewares';
-import { responseCookie } from '../helpers';
-import { STATUS_CODE } from '../constants';
-import { tryCatch } from '../helpers';
-import { UserLogin } from '../models';
+import { passport } from '../middlewares/passport_config';
+import { STATUS_CODE, STATUS_MESSAGE, SUCCESS_MESSAGE } from '../constants';
+import { User } from '../models';
 
 const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
-  return new Promise<{ user: UserLogin; info: { message?: string } }>(
-    (resolve, reject) => {
-      passport.authenticate(
-        'local',
-        (error: Error, user: UserLogin, info: { message?: string }) => {
-          if (error) {
-            return reject(
-              new CustomError(
-                'Internal server error',
-                STATUS_CODE.INTERNAL_SERVER_ERROR
-              )
-            );
-          }
-
-          if (!user) {
-            return reject(
-              new CustomError(
-                `Authentication failed, ${info.message}`,
-                STATUS_CODE.NOT_FOUND
-              )
-            );
-          }
-
-          resolve({ user, info });
-        }
-      )(req, res, next);
-    }
-  );
-};
-
-export const loginUser = tryCatch<{ message: '' }>(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { user } = await authenticateUser(req, res, next);
-
-    req.login(user, (error) => {
+  return new Promise<{ user: User; info: { message?: string } }>((resolve, reject) => {
+    passport.authenticate('local', (error: Error, user: User, info: { message?: string }) => {
       if (error) {
-        throw new CustomError(
-          'Login failed',
-          STATUS_CODE.INTERNAL_SERVER_ERROR
+        return reject(
+          new CustomError(STATUS_MESSAGE[STATUS_CODE.INTERNAL_ERROR], STATUS_CODE.INTERNAL_ERROR)
         );
       }
 
-      const maxAge = req.session?.cookie.maxAge!;
+      if (!user) {
+        return reject(
+          new CustomError(`Authentication failed, ${info?.message ?? ''}`, STATUS_CODE.NOT_FOUND)
+        );
+      }
 
-      responseCookie(res, maxAge);
+      resolve({ user, info });
+    })(req, res, next);
+  });
+};
 
-      return res.status(STATUS_CODE.OK).json({ success: true });
+export const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { user } = await authenticateUser(req, res, next);
+
+  req.login(user, (error) => {
+    if (error) throwError('Login failed', STATUS_CODE.INTERNAL_ERROR);
+
+    const maxAge = req.session?.cookie.maxAge!;
+
+    return res.status(STATUS_CODE.OK).json({
+      message: SUCCESS_MESSAGE['login'],
+      payload: { email: user.email, id: user._id, name: user.name },
+      success: true,
     });
-  }
-);
+  });
+};
