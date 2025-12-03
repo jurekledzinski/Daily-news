5dcf0d1614e765d7ed896f8c72775b7bd997e76a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 19:36:16 +0100

    Frontend: Changes url for navigate in navigateDetailsArticle

--- frontend/src/pages/grid-articles/hooks/navigate-to-details-article/useNavigateToDetailsArticle.tsx
+++ frontend/src/pages/grid-articles/hooks/navigate-to-details-article/useNavigateToDetailsArticle.tsx
@@ -1,11 +1,16 @@
+import { getLocalData } from '@/helpers';
 import { useNavigate } from 'react-router';
 
 export const useNavigateToDetailsArticle = () => {
   const navigate = useNavigate();
 
   const navigateDetailsArticle = (id: string) => {
-    const url = new URL(window.location.href);
-    navigate({ pathname: `${url.pathname}/${encodeURIComponent(id)}` });
+    const currentURL = new URL(window.location.href);
+    const page = getLocalData<Record<string, string>>('pages', 'object')[id];
+    navigate({
+      pathname: `${currentURL.pathname}/${encodeURIComponent(id)}`,
+      ...(page && { search: `?page=${page}` }),
+    });
   };
 
   return navigateDetailsArticle;
