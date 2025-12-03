481b06128803e310b62710957e0a9189b19678bf
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 19:23:31 +0100

    Frontend: Refactor actions for details article page

--- frontend/src/api/actions/detailsArticle.ts
+++ frontend/src/api/actions/detailsArticle.ts
@@ -1,97 +1,27 @@
-import { ActionCreateComment, ActionCreateCommentReply, ActionUpdateLikesComment } from './types';
-import { Comment, Likes } from '@models';
-import { createComment, updateLikesComment } from '../api-calls';
-import { formatDataToObject, queryInvalidate, validateAction } from './helpers';
-import { LoaderFunctionArgs, Params, redirect } from 'react-router';
-import { showSuccessToast } from '@helpers';
+import { ActionCreateComment } from './types';
+import { ActionFunction } from 'react-router';
+import { Comment } from '@models';
+import { createComment, setActionResponse } from '../api-calls';
+import { formatDataToObject, queryInvalidate } from './helpers';
 
-export const actionDetailsArticle =
-  () =>
-  async ({ params, request }: LoaderFunctionArgs<unknown>) => {
-    const { id } = params as Params;
-    const articleId = decodeURIComponent(id ?? '');
-    const url = new URL(request.url);
-    const page = url.searchParams.get('page') ?? '1';
-    const pageReply = url.searchParams.get('page_reply') ?? '1';
+export const actionDetailsArticle: ActionFunction = async ({ params, request }) => {
+  const articleId = decodeURIComponent(params.id ?? '');
+  const url = new URL(request.url);
+  const page = url.searchParams.get('page') ?? '1';
 
-    const data = await request.formData();
-    const actionType = data.get('actionType');
+  const data = await request.formData();
+  const actionType = data.get('actionType');
+  const ctx = { data, articleId, page };
 
-    if (actionType === 'create-comment') {
-      return actionCreateComment({ data, articleId, page });
-    } else if (actionType === 'create-reply') {
-      return actionCreateCommentReply({ data, articleId, page, pageReply: '1' });
-    } else if (actionType === 'update-likes') {
-      return actionUpdateLikesComment({ data, articleId, page, pageReply });
-    }
-  };
+  if (actionType === 'create-comment') return actionCreateComment(ctx);
+};
 
-export const actionCreateComment: ActionCreateComment = async ({ data, articleId, page }) => {
+export const actionCreateComment: ActionCreateComment = async ({ data, articleId }) => {
   data.delete('actionType');
   const newComment = formatDataToObject<Omit<Comment, 'id'>>(data);
   const result = await createComment(newComment);
 
-  queryInvalidate(['list-comments', articleId, page]);
-  queryInvalidate(['crsf-token']);
+  queryInvalidate(['list-comments', articleId]);
 
-  validateAction(result, 'create-comment');
-
-  showSuccessToast('Comment added successfully!');
-
-  return redirect(window.location.pathname);
-};
-
-export const actionCreateCommentReply: ActionCreateCommentReply = async ({
-  data,
-  articleId,
-  page,
-  pageReply,
-}) => {
-  data.delete('actionType');
-  const newReply = formatDataToObject<Omit<Comment, 'id'>>(data);
-  const result = await createComment(newReply);
-  const parentId = newReply.parentCommentId ?? '';
-
-  queryInvalidate(['list-comments', articleId, page]);
-  queryInvalidate(['list-comment-replies', articleId, pageReply, parentId]);
-  queryInvalidate(['crsf-token']);
-
-  validateAction(result, 'create-reply');
-
-  showSuccessToast('Comment added successfully!');
-
-  const redirectTo = `${window.location.pathname}?comment_id=${newReply.parentCommentId}`;
-
-  return redirect(redirectTo);
-};
-
-export const actionUpdateLikesComment: ActionUpdateLikesComment = async ({
-  data,
-  articleId,
-  page,
-  pageReply,
-}) => {
-  data.delete('actionType');
-  const comment = formatDataToObject<Likes>(data);
-  const result = await updateLikesComment({ articleId, body: comment });
-
-  if (comment.parentCommentId === 'null') {
-    const redirectTo = `${window.location.pathname}?page=${page}`;
-
-    validateAction(result, 'update-likes');
-
-    queryInvalidate(['list-comments', articleId, page]);
-
-    return redirect(redirectTo);
-  }
-
-  const redirectTo = `${window.location.pathname}?comment_id=${comment.parentCommentId}&page_reply=${pageReply}`;
-
-  validateAction(result, 'update-likes');
-
-  const parentId = comment.parentCommentId ?? '';
-
-  queryInvalidate(['list-comment-replies', articleId, pageReply, parentId]);
-
-  return redirect(redirectTo);
+  return setActionResponse('create-comment', result);
 };
