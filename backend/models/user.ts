fea52b901c77578d06b19a81338eaba1c394d384
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 20:11:02 +0100

    Backend: Update user zod schema

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -2,7 +2,8 @@ import { z } from 'zod';
 
 export const UserSchema = z.object({
   name: z.string().min(1, { message: 'Name is required' }),
-  email: z.string().email({ message: 'Email is invalid' }).min(1, { message: 'Email is required' }),
+  surname: z.string().min(1, { message: 'Surname is required' }),
+  email: z.email({ error: 'Email is invalid' }).min(1, { message: 'Email is required' }),
   password: z.string().min(8, { message: 'Password required at least 8 characters' }),
 });
 
