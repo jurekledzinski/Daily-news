cbab046201c4e44c8b92cea4997388e59f5f8389
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 19:51:40 +0100

    Frontend: Add types for actions profile and change name type for action comments

--- frontend/src/api/actions/types.ts
+++ frontend/src/api/actions/types.ts
@@ -1,6 +1,8 @@
 type Params = { data: FormData; articleId: string; page: string };
+type ParamsProfile = { data: FormData; id: string };
 
 type Action<T> = (params: T) => Promise<{ message?: string } | Response>;
 
 export type ActionFunctionData = Action<Pick<Params, 'data'>>;
-export type ActionCreateComment = Action<Params>;
+export type ActionComment = Action<Params>;
+export type ActionProfile = Action<ParamsProfile>;
