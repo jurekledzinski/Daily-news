7a5342a01cd7c449ccf821d9c46209961f37f985
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 21:26:52 +0100

    Frontend: Update types in useUserProfile hook

--- frontend/src/components/pages/profile/forms/hooks/user-profile/types.ts
+++ frontend/src/components/pages/profile/forms/hooks/user-profile/types.ts
@@ -1,5 +1,6 @@
 import { ActionData } from '@api';
 import { FetcherReset } from '@components/pages/home';
+import { User } from '@models';
 
 export type UserProfileFormValues = {
   email: string;
@@ -9,5 +10,5 @@ export type UserProfileFormValues = {
 
 export type UseUserProfileFormProps = {
   onFailed: (reset: FetcherReset, data?: ActionData<unknown>) => void;
-  onSuccess: (reset: FetcherReset, data?: ActionData<unknown>) => void;
+  onSuccess: (reset: FetcherReset, data?: ActionData<Pick<User, 'email' | 'name' | 'surname'>>) => void;
 };
--- frontend/src/components/pages/profile/forms/hooks/user-profile/useUserProfile.tsx
+++ frontend/src/components/pages/profile/forms/hooks/user-profile/useUserProfile.tsx
@@ -1,13 +1,14 @@
 import { ActionData } from '@api';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useFetcher } from 'react-router';
+import { User } from '@models';
 import { useResetForm } from '@hooks';
 import { UserProfileFormValues, UseUserProfileFormProps } from './types';
 
 export const useUserProfile = ({ onFailed, onSuccess }: UseUserProfileFormProps) => {
   const methods = useForm<UserProfileFormValues>({ defaultValues: { email: '', name: '', surname: '' } });
 
-  const fetcher = useFetcher<ActionData<unknown>>();
+  const fetcher = useFetcher<ActionData<Pick<User, 'email' | 'name' | 'surname'>>>();
 
   const onSubmit: SubmitHandler<UserProfileFormValues> = (data) => {
     const formData = new FormData();
