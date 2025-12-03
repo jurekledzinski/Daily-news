4aa04dc40dc733b4e0a7f64e5b09e5d7f17cd619
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 19:19:17 +0100

    Backend: Refactor comments zod schema

--- backend/models/comments.ts
+++ backend/models/comments.ts
@@ -1,22 +1,11 @@
-import { ObjectId } from 'mongodb';
 import { z } from 'zod';
 
-const CommentSchema = z.object({
-  createdAt: z.string(),
-  idArticle: z.string(),
-  likes: z.number(),
-  parentCommentId: z.string().nullable(),
+export const CommentSchema = z.object({
+  createdAt: z.date(),
+  articleId: z.string(),
   text: z.string(),
   user: z.string(),
   userId: z.string(),
 });
 
-const LikesSchema = CommentSchema.pick({ likes: true });
-
-type Comment = z.infer<typeof CommentSchema>;
-
-interface IComment extends Comment {
-  _id?: ObjectId;
-}
-
-export { CommentSchema, IComment, LikesSchema };
+export type Comment = z.infer<typeof CommentSchema>;
