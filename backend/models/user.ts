327232717a9d0d6bef8fd407ed34bb93125741c4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 19:20:16 +0100

    Backend: Refactor user zod schema

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,18 +1,9 @@
-import { ObjectId } from 'mongodb';
 import { z } from 'zod';
 
-const UserSchema = z.object({
-  _id: z.string().optional(),
+export const UserSchema = z.object({
   name: z.string().min(1, { message: 'Name is required' }),
   email: z.string().email({ message: 'Email is invalid' }).min(1, { message: 'Email is required' }),
   password: z.string().min(8, { message: 'Password required at least 8 characters' }),
 });
 
-type User = z.infer<typeof UserSchema>;
-
-const ChangeUserPasswordSchema = UserSchema.pick({ password: true });
-const UpdateUserProfileSchema = UserSchema.pick({ name: true, email: true });
-
-type UserData = Omit<User, '_id'> & { _id: ObjectId | string };
-
-export { ChangeUserPasswordSchema, UpdateUserProfileSchema, UserSchema, User, UserData };
+export type User = z.infer<typeof UserSchema>;
