9de98cafccdb64daebdd3e2f3e6b185732e73f4b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 19:28:22 +0100

    Frontend: Refactor comment form

--- frontend/src/components/pages/details-article/forms/comment/CommentForm.tsx
+++ frontend/src/components/pages/details-article/forms/comment/CommentForm.tsx
@@ -1,14 +1,6 @@
+import { Button, ButtonGroup, Field, Form, Message, TextareaInput, TextInput } from '@components/shared';
 import { CommentFormProps } from './types';
 import { faCommentAlt } from '@fortawesome/free-solid-svg-icons';
-import {
-  Button,
-  ButtonGroup,
-  Field,
-  Form,
-  Message,
-  TextInput,
-  TextareaInput,
-} from '@components/shared';
 
 export const CommentForm = ({ controls, isPending, onSubmit }: CommentFormProps) => {
   const { register, formState } = controls;
@@ -18,27 +10,25 @@ export const CommentForm = ({ controls, isPending, onSubmit }: CommentFormProps)
     <Form className="g-sm" id="loginForm" onSubmit={onSubmit} noValidate>
       <Field>
         <TextInput
-          autoComplete="username"
-          label="Name"
           {...register('user', { required: 'Name is required' })}
-          isPending={isPending}
+          autoComplete="username"about=''
+          label="Name"
           variant="outlined"
         />
         {errors.user && <Message>{errors.user.message}</Message>}
       </Field>
       <Field>
         <TextareaInput
+          {...register('text', { required: 'Comment is required' })}
           autoComplete="comment"
           label="Comment"
-          {...register('text', { required: 'Comment is required' })}
-          isPending={isPending}
           rows={5}
           variant="outlined"
         />
         {errors.text && <Message>{errors.text.message}</Message>}
       </Field>
       <ButtonGroup fullWidth justify="justify-end">
-        <Button iconEnd={[faCommentAlt]} label="Add comment" type="submit" />
+        <Button isLoading={isPending} iconEnd={[faCommentAlt]} label="Add comment" type="submit" />
       </ButtonGroup>
     </Form>
   );
--- frontend/src/components/pages/details-article/forms/hooks/comment/types.ts
+++ frontend/src/components/pages/details-article/forms/hooks/comment/types.ts
@@ -1,10 +1,15 @@
-import { Comment } from '@/models';
+import { ActionData } from '@api';
+import { Comment } from '@models';
+import { Navigation } from 'react-router';
 
 export type CommentFormValues = Pick<Comment, 'text' | 'user'>;
 
 export type UseCommentFormProps = {
   articleId: string;
+  onFailed: () => void;
   onSuccess: () => void;
-  status: 'idle' | 'loading' | 'submitting';
-  userId: string;
+  status: Navigation;
+  action?: ActionData;
+  token?: string;
+  userId?: string;
 };
