d8605f1f8c5e715172493bbd60548f3023f06793
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 20:26:36 +0100

    Backend: Update user profile contoller

--- backend/controllers/users.ts
+++ backend/controllers/users.ts
@@ -5,7 +5,7 @@ import { getCollectionDb } from '../config';
 import { NextFunction, Request, Response } from 'express';
 import { ObjectId } from 'mongodb';
 import { requestLogout } from '../helpers';
-import { STATUS_CODE, STATUS_MESSAGE } from '../constants';
+import { STATUS_CODE, STATUS_MESSAGE, SUCCESS_MESSAGE } from '../constants';
 import { throwError } from '../error';
 
 const collection = getCollectionDb<DataDB<User>>('users');
@@ -25,12 +25,12 @@ export const updateUserProfile = async (req: Request, res: Response) => {
 
   const parsedData = UserSchema.pick({ name: true, email: true }).parse(req.body);
 
-  const sessionUser = req.user! as User;
+  const sessionUser = req.user! as DataDB<User>;
 
-  const isExist = (await collection.findOne({ email: req.body.email })) !== null;
+  const existingUser = await collection.findOne({ email: parsedData.email });
 
-  if (isExist && sessionUser && sessionUser.email !== req.body.email) {
-    throwError('User exist', STATUS_CODE.CONFLICT);
+  if (existingUser && existingUser._id.toString() !== sessionUser._id?.toString()) {
+    throwError('User already exists', STATUS_CODE.CONFLICT);
   }
 
   await collection.updateOne(
@@ -38,7 +38,7 @@ export const updateUserProfile = async (req: Request, res: Response) => {
     { $set: { email: xss(parsedData.email), name: xss(parsedData.name) } }
   );
 
-  return res.status(STATUS_CODE.OK).json({ success: true });
+  return res.status(STATUS_CODE.OK).json({ message: SUCCESS_MESSAGE['updateProfile'], success: true });
 };
 
 export const changeUserPassword = async (req: Request, res: Response) => {
@@ -56,7 +56,7 @@ export const changeUserPassword = async (req: Request, res: Response) => {
 
   await collection.updateOne({ _id: new ObjectId(req.params.id) }, { $set: { password: hashedPassword } });
 
-  return res.status(STATUS_CODE.OK).json({ success: true });
+  return res.status(STATUS_CODE.OK).json({ message: SUCCESS_MESSAGE['updatePassword'], success: true });
 };
 
 export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -69,5 +69,5 @@ export const deleteUser = async (req: Request, res: Response, next: NextFunction
   const result = await collection.deleteOne({ _id: new ObjectId(id) });
 
   if (result.deletedCount) requestLogout(req, res, next);
-  else res.status(STATUS_CODE.OK).json({ success: false });
+  else res.status(STATUS_CODE.OK).json({ messge: SUCCESS_MESSAGE['deleteUser'], success: false });
 };
