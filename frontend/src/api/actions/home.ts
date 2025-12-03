c860902f03e9d3676377dde0754596b9a15fdb26
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-03 19:24:21 +0100

    Frontend: Refactor actions for home page

--- frontend/src/api/actions/home.ts
+++ frontend/src/api/actions/home.ts
@@ -1,8 +1,6 @@
 import { ActionFunctionData } from './types';
-import { formatDataToObject, queryInvalidate } from './helpers';
-import { invalidateQueryClient } from '@helpers';
+import { formatDataToObject } from './helpers';
 import { loginUser, logoutUser, registerUser, setActionResponse } from '../api-calls';
-import { queryClient } from '@routes';
 import { User } from '@models';
 import type { ActionFunction } from 'react-router';
 
@@ -32,15 +30,11 @@ export const actionLoginUser: ActionFunctionData = async ({ data }) => {
   const user = formatDataToObject<Omit<User, 'id' | 'name'>>(data);
   const result = await loginUser(user);
 
-  await invalidateQueryClient(queryClient, ['user']);
-
   return setActionResponse('login-user', result);
 };
 
 export const actionLogoutUser = async () => {
   const result = await logoutUser({});
-
-  queryInvalidate(['user']);
 
   return setActionResponse('logout-user', result);
 };
