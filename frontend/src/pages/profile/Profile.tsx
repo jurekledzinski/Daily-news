865eda8548144386163ba6430945b49da6d99426
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-04 18:18:15 +0100

    Frontend: Add Profile page and styles

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -1,3 +1,22 @@
+import styles from './Profile.module.css';
+import { ChangePassword, DangerZone, PersonalInformation } from '@components/pages';
+import { Heading } from '@/components/shared';
+import { useLoaderData } from 'react-router';
+
 export const Profile = () => {
-  return <div>Profile page</div>;
+  const loader = useLoaderData<{ data: string; success: boolean }>();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Heading className={styles.heading} level={4}>
+          Profile Settings
+        </Heading>
+        <p className={styles.text}>Manage your account information and preferences</p>
+        <PersonalInformation token={loader.data} />
+        <ChangePassword token={loader.data} />
+        <DangerZone token={loader.data} />
+      </div>
+    </div>
+  );
 };
