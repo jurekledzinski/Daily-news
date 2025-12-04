1253cddd52cd9281f2fd305ea1e11dc9c997c41d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-04 18:16:55 +0100

    Frontend: Add global helper function  defaultErrorMessage

--- frontend/src/helpers/formatText.ts
+++ frontend/src/helpers/formatText.ts
@@ -2,3 +2,6 @@ export const capitalizeFirstLetter = (text?: string) => {
   if (!text) return '';
   return text.charAt(0).toUpperCase() + text.slice(1);
 };
+
+export const defaultErrorMessage = (action: string) =>
+  `${capitalizeFirstLetter(action)} failed. Please try again later.`;
