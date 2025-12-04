9a4f3e6aba7dd0d7df5aa787cc94a540538d5458
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-04 18:13:11 +0100

    Frontend: Create hook useProfileCallbacks for define callbacks triggered in useUserProfile

--- frontend/src/components/pages/profile/hooks/profile-callbacks/useProfileCallbacks.tsx
+++ frontend/src/components/pages/profile/hooks/profile-callbacks/useProfileCallbacks.tsx
@@ -1,6 +1,6 @@
 import { ActionData } from '@api';
+import { defaultErrorMessage, showErrorToast, showSuccessToast } from '@helpers';
 import { FetcherReset } from '@components/pages/home';
-import { removeCookie, showErrorToast, showSuccessToast } from '@helpers';
 import { User } from '@models';
 import { useUserStore } from '@store';
 
@@ -11,50 +11,21 @@ export const useProfileCallbacks = () => {
     reset: FetcherReset,
     data?: ActionData<Pick<User, 'email' | 'name' | 'surname'>>
   ) => {
-    if (!data || !data.payload) return;
-    dispatch({ type: 'SET_USER', payload: data.payload });
-    showSuccessToast(data.message);
-    reset();
-  };
-
-  const successUpdatePassword = (reset: FetcherReset, data?: ActionData<unknown>) => {
-    if (!data) return;
+    if (!data) return showErrorToast(defaultErrorMessage('update profile'));
+    if (data.payload) dispatch({ type: 'SET_USER', payload: data.payload });
     showSuccessToast(data.message);
     reset();
   };
 
   const failedUpdateProfile = (reset: FetcherReset, data?: ActionData<unknown>) => {
-    if (!data) return;
-    showErrorToast(data.message);
-    reset();
-  };
-
-  const failedUpdatePassword = (reset: FetcherReset, data?: ActionData<unknown>) => {
-    if (!data) return;
-    showErrorToast(data.message);
-    reset();
-  };
-
-  const failedDeleteAccount = (reset: FetcherReset, data?: ActionData<unknown>) => {
-    if (!data) return;
-    removeCookie('enable');
+    if (!data) return showErrorToast(defaultErrorMessage('update profile'));
     showErrorToast(data.message);
     reset();
   };
 
   return {
     failedUpdateProfile,
-    failedUpdatePassword,
-    failedDeleteAccount,
     successUpdateProfile,
-    successUpdatePassword,
     state,
   };
 };
-
-// Praktycznie to co w useAuthCallbacks
-// Możliwe że po update trzeba będzie jednak wysłać coś z powrotem gdy update user
-// name surname email by zaktualizować store user wtedy gdy update user
-
-// A gdy delete user accoutn to dispatch user logout będzie tam
-// Gdy delete user account to na backend dodaj usuwanie wszystkich komentarzy usera także
