3b339aedff58c4139784157c8c52b9ffc1d16af5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-04 11:32:53 +0100

    Frontend: Add cookie check in protected route component

--- frontend/src/components/shared/protected-route/ProtectedRoute.tsx
+++ frontend/src/components/shared/protected-route/ProtectedRoute.tsx
@@ -1,11 +1,13 @@
+import { getCookie } from '@/helpers';
 import { Navigate } from 'react-router';
 import { ProtectedRouteProps } from './types';
 import { useUserStore } from '@/store';
 
 export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const data = useUserStore();
+  const enabled = getCookie('enable');
 
-  if (!data.state.user) {
+  if (!data.state.user && !enabled) {
     return <Navigate to="/" replace />;
   }
 
