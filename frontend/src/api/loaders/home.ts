4da5e3045ab63c3b8cd7f375cebfe348a5eaf229
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-05 16:42:51 +0100

    Frontend: Refactor loaders for home page

--- frontend/src/api/loaders/home.ts
+++ frontend/src/api/loaders/home.ts
@@ -1,33 +1,29 @@
 import { APIErrorResponse, APISuccessResponse } from '../api';
 import { asyncStoragePersister, queryClient } from '@routes';
 import { fetchApi } from '../api-calls';
+import { loaderTryCatch } from '@/helpers';
+import { persistQueryClientRestore, persistQueryClientSave } from '@tanstack/query-persist-client-core';
 import { URLS } from '../urls';
 import type { LoaderFunction } from 'react-router';
 import type { Section } from '@guardian/content-api-models/v1/section';
-import {
-  persistQueryClientRestore,
-  persistQueryClientSave,
-} from '@tanstack/query-persist-client-core';
+
+export const loaderHome = async () => {
+  await persistQueryClientRestore({ queryClient: queryClient, persister: asyncStoragePersister });
+
+  const data = await queryClient.ensureQueryData<APISuccessResponse<Section[]>, APIErrorResponse>({
+    queryKey: ['list-categories'],
+    queryFn: async () => {
+      const response = await fetchApi({ url: URLS.GET_CATEGORIES_ARTICLES() });
+      return response.response.results;
+    },
+    revalidateIfStale: false,
+  });
+
+  await persistQueryClientSave({ queryClient, persister: asyncStoragePersister });
+
+  return { data: data ?? [], success: true };
+};
 
 export const loaderHomePage: LoaderFunction = async () => {
-  try {
-    await persistQueryClientRestore({ queryClient: queryClient, persister: asyncStoragePersister });
-
-    const data = await queryClient.ensureQueryData<APISuccessResponse<Section[]>, APIErrorResponse>(
-      {
-        queryKey: ['list-categories'],
-        queryFn: async () => {
-          const response = await fetchApi({ url: URLS.GET_CATEGORIES_ARTICLES() });
-          return response.response.results;
-        },
-        revalidateIfStale: false,
-      }
-    );
-
-    await persistQueryClientSave({ queryClient, persister: asyncStoragePersister });
-
-    return { data, success: true };
-  } catch {
-    return { success: false };
-  }
+  return await loaderTryCatch(loaderHome());
 };
