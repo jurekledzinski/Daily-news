fed4c4e07d09ce13f91eae177ec3dabf07842e20
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-05 16:42:38 +0100

    Frontend: Refactor loaders for details articles

--- frontend/src/api/loaders/article.ts
+++ frontend/src/api/loaders/article.ts
@@ -1,32 +1,36 @@
 import { APIErrorResponse, APISuccessResponse, fetchApi, URLS } from '@api';
-import { findTheBiggestImageInArticle, getCookie } from '@helpers';
-import { queryClient } from '@routes';
+import { asyncStoragePersister, queryClient } from '@routes';
+import { findTheBiggestImageInArticle, getCookie, loaderTryCatch } from '@helpers';
+import { persistQueryClientRestore, persistQueryClientSave } from '@tanstack/query-persist-client-core';
 import type { LoaderFunction, Params } from 'react-router';
 import type { Content } from '@guardian/content-api-models/v1/content';
 
-const loadDetailsArticles = async (params: Params<string>) => {
+const loadDetails = async (params: Params<string>) => {
   const category = params.category ?? '';
   const id = decodeURIComponent(params.id ?? '');
 
-  try {
-    const data = await queryClient.ensureQueryData<APISuccessResponse<Content>, APIErrorResponse>({
-      queryKey: ['details-article', category, id],
-      queryFn: async () => {
-        const response = await fetchApi({ url: URLS.GET_DETAILS_ARTICLE(id) });
-        return { payload: response.response.content, success: true };
-      },
-    });
-    const biggestImage = findTheBiggestImageInArticle(data);
-    const formattedData = { ...data.payload, image: biggestImage };
+  await persistQueryClientRestore({ queryClient: queryClient, persister: asyncStoragePersister });
 
-    return { data: formattedData, success: true };
-  } catch {
-    return { success: false };
-  }
+  const data = await queryClient.ensureQueryData<APISuccessResponse<Content>, APIErrorResponse>({
+    queryKey: ['details-article', category, id],
+    queryFn: async () => {
+      const response = await fetchApi({ url: URLS.GET_DETAILS_ARTICLE(id) });
+      return { payload: response.response.content, success: true };
+    },
+  });
+
+  await persistQueryClientSave({ queryClient, persister: asyncStoragePersister });
+
+  const biggestImage = findTheBiggestImageInArticle(data);
+  const formattedData = { ...data.payload, image: biggestImage };
+
+  if ('message' in data) return { success: false };
+  return { data: formattedData, success: true };
 };
 
-const loadToken = async () => {
+export const loadToken = async () => {
   const enabled = getCookie('enable');
+
   if (!enabled) return { success: false };
 
   const data = await fetchApi({ url: URLS.GET_CSRF_TOKEN() });
@@ -37,7 +41,7 @@ const loadToken = async () => {
 
 export const loaderArticleDetailsPage: LoaderFunction = async ({ params }) => {
   return {
-    article: await loadDetailsArticles(params),
-    token: await loadToken(),
+    article: await loaderTryCatch(loadDetails(params)),
+    token: await loaderTryCatch(loadToken()),
   };
 };
