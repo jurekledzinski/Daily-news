21c8a3071bbc9226ea1e60b1ff42a36a627236a8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-05 16:43:01 +0100

    Frontend: Refactor loaders for profile page

--- frontend/src/api/loaders/profile.ts
+++ frontend/src/api/loaders/profile.ts
@@ -1,18 +1,7 @@
-import { fetchApi } from '../api-calls';
-import { getCookie } from '@helpers';
 import { LoaderFunction } from 'react-router';
-import { URLS } from '../urls';
-
-const loadToken = async () => {
-  const enabled = getCookie('enable');
-  if (!enabled) return { success: false };
-
-  const data = await fetchApi({ url: URLS.GET_CSRF_TOKEN() });
-
-  if ('message' in data) return { success: false };
-  return { data: data.payload, success: true };
-};
+import { loaderTryCatch } from '@/helpers';
+import { loadToken } from './article';
 
 export const loaderProfilePage: LoaderFunction = async () => {
-  return await loadToken();
+  return await loaderTryCatch(loadToken());
 };
