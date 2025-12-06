37ded1fc7f506e69788ccc01babc063c0389ab5c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-06 21:56:39 +0100

    Frontend: Add CarouselThumbnails to TabsCategories

--- frontend/src/components/pages/categories-articles/tabs-categories/TabsCategories.tsx
+++ frontend/src/components/pages/categories-articles/tabs-categories/TabsCategories.tsx
@@ -1,6 +1,7 @@
+import { CarouselThumbnails, Tab, Tabs, TabsList, TabsPanel } from '@components/shared';
 import { Outlet } from 'react-router';
 import { Section } from '@guardian/content-api-models/v1/section';
-import { Tab, Tabs, TabsList, TabsPanel } from '@components/shared';
+import { SplideSlide } from '@splidejs/react-splide';
 import { tablistClassNames } from './utils';
 import { TabsCategoriesProps } from './types';
 import { useMemo } from 'react';
@@ -21,10 +22,16 @@ export const TabsCategories = ({ categories, category, navigateCategory }: TabsC
     >
       <div ref={ref}></div>
       <TabsList className={classNames}>
-        {(categories ?? []).map((category) => {
-          const content = JSON.parse(category.content!) as Section;
-          return <Tab key={category.id} id={category.id} label={content.webTitle} />;
-        })}
+        <CarouselThumbnails>
+          {(categories ?? []).map((category) => {
+            const content = JSON.parse(category.content!) as Section;
+            return (
+              <SplideSlide key={category.id} aria-hidden="false">
+                <Tab key={category.id} id={category.id} label={content.webTitle} />
+              </SplideSlide>
+            );
+          })}
+        </CarouselThumbnails>
       </TabsList>
       <TabsPanel>
         <Outlet />
