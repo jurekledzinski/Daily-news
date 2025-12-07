9fa6a5032fdfc61a43114cb3515b18716bbed74b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-07 16:01:30 +0100

    Frontend: Extend react splide js types

--- frontend/src/dts/splidejs.d.ts
+++ frontend/src/dts/splidejs.d.ts
@@ -1,1 +1,10 @@
 declare module '@splidejs/react-splide';
+
+declare module 'react-splidejs' {
+  import type { EventMap, Options, Splide } from '@splidejs/splide';
+  export type SplideOptions = Options;
+  export type onReady = (
+    splide: Splide,
+    ...args: Parameters<EventMap['ready']>
+  ) => ReturnType<EventMap['ready']>;
+}
