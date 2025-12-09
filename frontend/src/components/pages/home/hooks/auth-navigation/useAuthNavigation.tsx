644133b520376e2f62bde73aec95e4ab02beca8c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-09 03:51:30 +0100

    Frontend: Clear use query cache on logout

--- frontend/src/components/pages/home/hooks/auth-navigation/useAuthNavigation.tsx
+++ frontend/src/components/pages/home/hooks/auth-navigation/useAuthNavigation.tsx
@@ -1,15 +1,29 @@
+import { asyncStoragePersister, queryClient } from '@routes';
+import { clear } from 'idb-keyval';
 import { showSuccessToast } from '@helpers';
+import { useRef } from 'react';
 import { useMatch, useNavigate } from 'react-router';
-import { useUserStore } from '@/store';
+import { useUserStore } from '@store';
 
 export const useAuthNavigation = () => {
+  const timeoutId = useRef<ReturnType<typeof setTimeout>>(null);
   const navigate = useNavigate();
   const matchProfile = useMatch('/profile/:id');
   const { dispatch, state } = useUserStore();
 
-  const navigateLogout = () => {
+  const navigateLogout = async () => {
+    queryClient.clear();
+    await asyncStoragePersister.removeClient();
+
+    timeoutId.current = setTimeout(async () => {
+      await clear();
+      if (timeoutId.current) clearTimeout(timeoutId.current);
+    }, 1500);
+
     dispatch({ type: 'LOGOUT_USER' });
     showSuccessToast('Logout successful');
+    localStorage.clear();
+
     if (matchProfile) navigate('/', { replace: true, viewTransition: true });
     else navigate(window.location.pathname, { replace: true, viewTransition: true });
   };
