a0e5bb0216e895daa44a4a575c6b274a715153f7
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-09 06:37:26 +0100

    Frontend: Remove not used helper getCurrentCategory

--- frontend/src/helpers/localStorage.ts
+++ frontend/src/helpers/localStorage.ts
@@ -1,14 +1,8 @@
-import { GridStackNode } from 'gridstack';
-
 export const getLocalData = <T>(name: string, type: 'array' | 'string' | 'object' = 'array') => {
   const constructor = { array: '[]', string: '""', object: '{}' }[type];
   const tempData = localStorage.getItem(name) ?? constructor;
   const localData: T = JSON.parse(tempData);
   return localData;
-};
-
-export const getCurrentCategory = <T extends GridStackNode>(category: string) => {
-  return getLocalData<T[]>('categories').find((i) => i.id === category);
 };
 
 export const removeLocalData = (key: string) => {
