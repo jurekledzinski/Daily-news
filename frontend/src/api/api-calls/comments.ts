708b5a8ae10d2dc5b8246f7d3cd9f8c2f8aa030c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-09 06:35:46 +0100

    Frontend format by prettier comments api calls file

--- frontend/src/api/api-calls/comments.ts
+++ frontend/src/api/api-calls/comments.ts
@@ -4,19 +4,17 @@ import { fetchApi } from './helpers';
 import { tryCatch } from '@helpers';
 import { URLS } from '../urls';
 
-export const createComment = tryCatch<
-  Omit<APISuccessResponse, 'payload'>,
-  APIErrorResponse,
-  Omit<Comment, 'id'>
->(async (data) => {
-  const { csrfToken, ...body } = data;
+export const createComment = tryCatch<Omit<APISuccessResponse, 'payload'>, APIErrorResponse, Omit<Comment, 'id'>>(
+  async (data) => {
+    const { csrfToken, ...body } = data;
 
-  return await fetchApi({
-    url: URLS.CREATE_COMMENT(),
-    method: 'POST',
-    mode: 'cors',
-    credentials: 'include',
-    body: JSON.stringify(body),
-    csrfToken,
-  });
-});
+    return await fetchApi({
+      url: URLS.CREATE_COMMENT(),
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      body: JSON.stringify(body),
+      csrfToken,
+    });
+  }
+);
