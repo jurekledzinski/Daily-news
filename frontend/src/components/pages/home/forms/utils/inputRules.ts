d12ddbe51a06b91a2663bf89c1135a96642d4859
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-11 14:38:25 +0100

    Frontend: add utils folder and inputRules file for register form

---
+++ frontend/src/components/pages/home/forms/utils/inputRules.ts
@@ -0,0 +1,20 @@
+import { RegisterFormValues } from '../hooks';
+import { Validate } from 'react-hook-form';
+
+export const passwordRules: Record<string, Validate<string, RegisterFormValues>> = {
+  hasNoSpace: (v) => /^\S*$/.test(v) || 'Must not contain spaces',
+  hasNumber: (v) => /\d/.test(v) || 'Must include at least one number',
+  hasUppercase: (v) => /[A-Z]/.test(v) || 'Must include at least one uppercase letter',
+  hasLowercase: (v) => /[a-z]/.test(v) || 'Must include at least one lowercase letter',
+  hasSpecial: (v) => /[@#!]/.test(v) || 'Must include at least one special character: @#!',
+  minLength: (v) => v.length >= 8 || 'Password must be at least 8 characters',
+};
+
+export const confirmPasswordRules: Record<string, Validate<string | undefined, RegisterFormValues>> = {
+  sameValues: (_, rest) => rest.password === rest.confirmPassword || 'Passwords do not match',
+};
+
+export const emailRules: Record<string, Validate<string, RegisterFormValues>> = {
+  matchPattern: (v) =>
+    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Email address must be a valid address',
+};
