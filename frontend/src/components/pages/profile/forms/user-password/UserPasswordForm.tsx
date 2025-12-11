267eb30a940d7702635c40b0bc14d55694e59c08
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-11 16:27:32 +0100

    Frontend: Update user password form and add password validation status component

--- frontend/src/components/pages/profile/forms/user-password/UserPasswordForm.tsx
+++ frontend/src/components/pages/profile/forms/user-password/UserPasswordForm.tsx
@@ -1,32 +1,61 @@
-import { Button, ButtonGroup, Field, Form, Message, PasswordInput } from '@components/shared';
+import { confirmPasswordRules, passwordRules } from '../utils';
 import { UserPasswordFormProps } from './types';
+import {
+  Button,
+  ButtonGroup,
+  Field,
+  Form,
+  Message,
+  PasswordInput,
+  PasswordValidationStatus,
+} from '@components/shared';
 
 export const UserPasswordForm = ({ controls, isPending, onSubmit }: UserPasswordFormProps) => {
-  const { register, formState } = controls;
+  const { control, register, formState, getValues, trigger } = controls;
   const { errors } = formState;
 
   return (
-    <Form className="g-sm" onSubmit={onSubmit} noValidate>
+    <Form autoComplete="off" className="g-sm" onSubmit={onSubmit} noValidate>
       <Field>
         <PasswordInput
-          autoComplete="new-password"
+          autoComplete="off"
           label="Password"
-          {...register('password', { required: 'Password is required' })}
+          {...register('password', { required: 'Password is required', validate: passwordRules })}
           variant="outlined"
         />
         {errors.password && <Message>{errors.password.message}</Message>}
       </Field>
       <Field>
         <PasswordInput
-          autoComplete="current-password"
+          autoComplete="off"
           label="Confirm password"
-          {...register('confirmPassword', { required: 'Confirm password is required' })}
+          {...register('confirmPassword', {
+            required: 'Confirm password is required',
+            validate: confirmPasswordRules,
+          })}
           variant="outlined"
         />
         {errors.confirmPassword && <Message>{errors.confirmPassword.message}</Message>}
       </Field>
+
+      <PasswordValidationStatus
+        control={control}
+        getValues={getValues}
+        nameConfirmPassword="confirmPassword"
+        namePassword="password"
+        passwordRules={passwordRules}
+        trigger={trigger}
+      />
+
       <ButtonGroup justify="justify-end" fullWidth>
-        <Button color="info" label="Change password" isLoading={isPending} type="submit" />
+        <Button
+          className="r-xs"
+          color="info"
+          label="Change password"
+          isLoading={isPending}
+          size="size-xs"
+          type="submit"
+        />
       </ButtonGroup>
     </Form>
   );
