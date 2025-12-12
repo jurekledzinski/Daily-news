3815fed1e4203fd6a6a9dfefebaedde4db6f12bd
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-12-12 09:30:08 +0100

    Frontend: Add loader prop to image component

--- frontend/src/components/shared/image/types.ts
+++ frontend/src/components/shared/image/types.ts
@@ -1,6 +1,8 @@
 import { ImgHTMLAttributes } from 'react';
+import { Loading } from './components';
 
 export type ImageProps = ImgHTMLAttributes<HTMLImageElement>;
 export type ImageWithLoaderProps = {
   src: string;
+  loader: Loading;
 };
